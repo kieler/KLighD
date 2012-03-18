f2664edc7f05a4ada90366e5c9e120e1a1032001
Christian Schneider <chsch@informatik.uni-kiel.de>
2012-03-18 12:45:36 +0100

    Java5 compatibility of KGraphTransientValueService established (kgraph.text)

--- plugins/de.cau.cs.kieler.core.kgraph.text/src/de/cau/cs/kieler/core/kgraph/text/serializer/KGraphTransientValueService.java
+++ plugins/de.cau.cs.kieler.core.kgraph.text/src/de/cau/cs/kieler/core/kgraph/text/serializer/KGraphTransientValueService.java
@@ -1,3 +1,16 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ * 
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2012 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
 package de.cau.cs.kieler.core.kgraph.text.serializer;
 
 import org.eclipse.emf.ecore.EObject;
@@ -54,13 +67,16 @@ public class KGraphTransientValueService extends TransientValueService implement
     // the above methods implement
     // org.eclipse.xtext.parsetree.reconstr.ITransientValueService
     // we need this since the current IConcreteSyntaxValidator relies on it
-
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean isCheckElementsIndividually(final EObject owner, final EStructuralFeature feature) {
         return false;
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean isTransient(final EObject owner, final EStructuralFeature feature,
             final int index) {
         if (feature == KGraphPackage.eINSTANCE.getKNode_Parent()
