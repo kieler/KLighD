2a69dda06658417b9618a9baa1e387c17b78c568
Martin Riess <mri@informatik.uni-kiel.de>
2012-04-04 18:35:29 +0200

    Added missing file IGraphElement

---
+++ plugins/de.cau.cs.kieler.klighd.piccolo/src/de/cau/cs/kieler/klighd/piccolo/krendering/IGraphElement.java
@@ -0,0 +1,34 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2012 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.klighd.piccolo.krendering;
+
+import de.cau.cs.kieler.core.kgraph.KGraphElement;
+
+/**
+ * The interface for Piccolo nodes representing a {@code KGraphElement}.
+ * 
+ * @author mri
+ * 
+ * @param <T> the type of the graph element
+ */
+public interface IGraphElement<T extends KGraphElement> {
+
+    /**
+     * Returns the graph element represented by this node.
+     * 
+     * @return the graph element
+     */
+    T getGraphElement();
+    
+}
