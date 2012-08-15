99336caaaf04d9000cae580e894040e35b25960b
sgu <sgu@informatik.uni-kiel.de>
2012-08-15 14:24:02 +0200

    kgraph.text: Improved KGraphResource s.t. KNodes, KEdges, KPorts, and KLabels are properly "post-initialized" with KLayoutData after the parsing step, if they aren't already.

--- plugins/de.cau.cs.kieler.core.kgraph.text/src/de/cau/cs/kieler/core/kgraph/text/KGraphResource.java
+++ plugins/de.cau.cs.kieler.core.kgraph.text/src/de/cau/cs/kieler/core/kgraph/text/KGraphResource.java
@@ -23,8 +23,15 @@ import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.linking.lazy.LazyLinkingResource;
 import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.resource.SaveOptions;
+import org.eclipse.xtext.xbase.lib.IteratorExtensions;
 
+import com.google.common.collect.Iterators;
+
+import de.cau.cs.kieler.core.kgraph.KGraphElement;
+import de.cau.cs.kieler.core.kgraph.KGraphPackage;
 import de.cau.cs.kieler.core.kgraph.KNode;
+import de.cau.cs.kieler.kiml.klayoutdata.KEdgeLayout;
+import de.cau.cs.kieler.kiml.klayoutdata.KShapeLayout;
 import de.cau.cs.kieler.kiml.util.KimlUtil;
 
 /**
@@ -48,6 +55,7 @@ public class KGraphResource extends LazyLinkingResource {
             EObject o = this.getContents().get(0);
             if (o instanceof KNode) {
                 KimlUtil.loadDataElements((KNode) o);
+                ensureKGraphElementsInitialization((KNode) o);
             }
         }
     }
@@ -65,7 +73,9 @@ public class KGraphResource extends LazyLinkingResource {
         
         EObject refreshed = NodeModelUtils.findActualSemanticObjectFor(NodeModelUtils
                 .findLeafNodeAtOffset(this.getParseResult().getRootNode(), offset));
-        KimlUtil.loadDataElements((KNode) EcoreUtil2.getRootContainer(refreshed));
+        KNode node = (KNode) EcoreUtil2.getRootContainer(refreshed);
+        KimlUtil.loadDataElements(node);
+        ensureKGraphElementsInitialization(node);
     }
     
     /**
@@ -86,5 +96,30 @@ public class KGraphResource extends LazyLinkingResource {
         //  out-dated parse tree constructor
         super.doSave(outputStream, SaveOptions.newBuilder().format().noValidation().getOptions()
                 .toOptionsMap());
+    }
+    
+    /**
+     * Ensure proper initialization of KEdges. 
+     * 
+     * @param node the root KNode to perfom on
+     */
+    private void ensureKGraphElementsInitialization(final KNode node) {
+
+        for (KGraphElement e : IteratorExtensions.toIterable(Iterators.filter(node.eAllContents(),
+                KGraphElement.class))) {
+            if (KGraphPackage.eINSTANCE.getKNode().isInstance(e)
+                    || KGraphPackage.eINSTANCE.getKPort().isInstance(e)
+                    || KGraphPackage.eINSTANCE.getKLabel().isInstance(e)) {
+                if (e.getData(KShapeLayout.class) == null) {
+                    e.getData().add(KimlUtil.createInitializedNode().getData(KShapeLayout.class));
+                }
+            }
+
+            if (KGraphPackage.eINSTANCE.getKEdge().isInstance(e)) {
+                if (e.getData(KEdgeLayout.class) == null) {
+                    e.getData().add(KimlUtil.createInitializedEdge().getData(KEdgeLayout.class));
+                }
+            }
+       }
     }
 }
