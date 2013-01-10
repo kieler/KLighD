8b3049b9c4c0d34b5670c4c2b08d25b6c4bffee3
Alexander Koch <akoc@informatik.uni-kiel.de>
2013-01-10 13:53:22 +0100

    krendering.impl: added KExpandCollapseActionImpl

---
+++ plugins/de.cau.cs.kieler.core.krendering/src/de/cau/cs/kieler/core/krendering/impl/KExpandCollapseActionImpl.java
@@ -0,0 +1,50 @@
+/**
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ * 
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2012 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.core.krendering.impl;
+
+import de.cau.cs.kieler.core.krendering.KExpandCollapseAction;
+import de.cau.cs.kieler.core.krendering.KRenderingPackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>KExpand Collapse Action</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class KExpandCollapseActionImpl extends KActionImpl implements KExpandCollapseAction {
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected KExpandCollapseActionImpl() {
+        super();
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    protected EClass eStaticClass() {
+        return KRenderingPackage.Literals.KEXPAND_COLLAPSE_ACTION;
+    }
+
+} //KExpandCollapseActionImpl
