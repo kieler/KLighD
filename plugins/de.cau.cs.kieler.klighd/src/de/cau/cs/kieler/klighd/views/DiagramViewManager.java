b0739a9a79489a481d98e4a023b0d4d15046a474
Christian Schneider <chsch@informatik.uni-kiel.de>
2013-02-18 16:20:17 +0100

    klighd: removed debugging code from DiagramViewManager as the issue appears to be solved

--- plugins/de.cau.cs.kieler.klighd/src/de/cau/cs/kieler/klighd/views/DiagramViewManager.java
+++ plugins/de.cau.cs.kieler.klighd/src/de/cau/cs/kieler/klighd/views/DiagramViewManager.java
@@ -234,13 +234,6 @@ public final class DiagramViewManager implements IPartListener {
                                 page.showView(PRIMARY_VIEW_ID, id, IWorkbenchPage.VIEW_VISIBLE);
                         if (view instanceof DiagramViewPart) {
                             diagramView = (DiagramViewPart) view;
-                            if (diagramView.isDisposed()) {
-                                // this is some long term debugging stuff
-                                //  (see also getView(...)
-                                StatusManager.getManager().handle(
-                                        new Status(IStatus.ERROR, KlighdPlugin.PLUGIN_ID, "hier"),
-                                        StatusManager.SHOW);
-                            }
                         } else {
                             throw new PartInitException(
                                     "KLighD: Creation of a KLighD DiagramViewPart failed.\n"
