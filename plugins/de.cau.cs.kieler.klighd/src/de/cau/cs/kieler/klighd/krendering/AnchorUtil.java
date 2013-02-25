ce3a5898a3e4afb1dfbd9d7e05bd6dcd84a7c8d1
Alexander Koch <akoc@informatik.uni-kiel.de>
2013-02-25 15:39:28 +0100

    de.cau.cs.kieler.klighd.krendering: implemented parts of functionality to move end points of edges to the lines of the rendering they are attached to

--- plugins/de.cau.cs.kieler.klighd/src/de/cau/cs/kieler/klighd/krendering/AnchorUtil.java
+++ plugins/de.cau.cs.kieler.klighd/src/de/cau/cs/kieler/klighd/krendering/AnchorUtil.java
@@ -13,6 +13,7 @@
  */
 package de.cau.cs.kieler.klighd.krendering;
 
+import de.cau.cs.kieler.core.krendering.KEllipse;
 import de.cau.cs.kieler.core.krendering.KRendering;
 import de.cau.cs.kieler.core.krendering.KRenderingPackage;
 import de.cau.cs.kieler.core.krendering.KRoundedRectangle;
@@ -55,6 +56,8 @@ public final class AnchorUtil {
                 anchorPointRoundedRectangle(point, width, height, roundedRectangle.getCornerWidth(),
                         roundedRectangle.getCornerHeight());
                 break;
+            case KRenderingPackage.KELLIPSE:
+                anchorPointEllipse(point, width, height);
             default:
                 anchorPointRectangle(point, width, height);
             }
@@ -104,7 +107,46 @@ public final class AnchorUtil {
      */
     public static void anchorPointRoundedRectangle(final KVector point, final double rectWidth,
             final double rectHeight, final double cornerWidth, final double cornerHeight) {
-        // TODO
+//        TODO
+//        point.x = 10;
+//        point.y = 10;
     }
 
+    /**
+     * Move anchorPoints of edges that are connected to ellipses to make the edge end on the line of the 
+     * Rendering.
+     * @param point the current end point of the edge to be changed by this method
+     * @param width the width of the ellipse
+     * @param height the height of the ellipse
+     */
+    public static void anchorPointEllipse(final KVector point, final double width, final double height) {
+        
+        //keep in mind that the radius of the ellipse is also the centerPoint of the ellipse
+        double xRad = width / 2;
+        double yRad = width / 2;
+        
+        //calculate the direction to the center of the rendering
+        double xDir = point.x - xRad;
+        double yDir = point.y - yRad;
+        
+        //calculate distance of point to center
+        
+        double xScale = width / height;
+        double yScale = height / width;
+        
+        if (point.x == 0) {
+            //edge is attached to the left side: calculate where the line is for the given y coordinate
+            //and manipulate x coordinate accordingly
+            point.x = xRad - Math.sqrt(xRad * yRad - Math.pow(yRad - point.y * xScale, 2));
+        } else if (point.x == width) {
+            point.x = xRad + Math.sqrt(xRad * yRad - Math.pow(yRad - point.y * xScale, 2));
+        } else if (point.y == 0) {
+            //TODO
+            //manipulate y coordinate
+//          point.y = yRad - Math.sqrt(xRad * yRad - Math.pow((point.x - xRad), 2));
+//          point.y = yRad - Math.sqrt(xRad * yRad - Math.pow(xRad-point.x*xScale, 2));
+        } else if (point.y == height) {
+//          point.y = Math.sqrt(xRad * yRad - Math.pow(xRad - point.x * yScale, 2));
+        }
+     }
 }
