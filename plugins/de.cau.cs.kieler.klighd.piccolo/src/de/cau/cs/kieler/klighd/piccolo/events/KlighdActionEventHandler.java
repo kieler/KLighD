86af67c74006542061f4eb032991665237c2adc7
Christian Schneider <chsch@informatik.uni-kiel.de>
2013-04-28 11:09:04 +0200

    klighd.piccolo: updated KlighdActionEventHandler according to change of IAction

--- plugins/de.cau.cs.kieler.klighd.piccolo/src/de/cau/cs/kieler/klighd/piccolo/events/KlighdActionEventHandler.java
+++ plugins/de.cau.cs.kieler.klighd.piccolo/src/de/cau/cs/kieler/klighd/piccolo/events/KlighdActionEventHandler.java
@@ -15,11 +15,13 @@ package de.cau.cs.kieler.klighd.piccolo.events;
 
 import com.google.common.base.Predicate;
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 
 import de.cau.cs.kieler.core.krendering.KAction;
 import de.cau.cs.kieler.core.krendering.KRendering;
 import de.cau.cs.kieler.core.krendering.Trigger;
+import de.cau.cs.kieler.kiml.config.ILayoutConfig;
 import de.cau.cs.kieler.klighd.IAction.ActionContext;
 import de.cau.cs.kieler.klighd.LightDiagramServices;
 import de.cau.cs.kieler.klighd.actions.CollapseExpandAction;
@@ -28,7 +30,6 @@ import de.cau.cs.kieler.klighd.piccolo.krendering.viewer.KlighdMouseEventListene
 import de.cau.cs.kieler.klighd.piccolo.krendering.viewer.PiccoloViewer;
 import de.cau.cs.kieler.klighd.triggers.KlighdStatusTrigger;
 import de.cau.cs.kieler.klighd.triggers.KlighdStatusTrigger.KlighdStatusState;
-import de.cau.cs.kieler.klighd.views.DiagramViewManager;
 import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.event.PInputEventListener;
 
@@ -74,22 +75,22 @@ public class KlighdActionEventHandler implements PInputEventListener {
             }
             
             ActionContext context = null; // construct the context lazily when it is required
+            ILayoutConfig config = null;
             
             viewer.setRecording(true);
-            
             for (KAction action : Iterables.filter(rendering.getActions(), WELLFORMED)) {
                 if (action.getTrigger().equals(Trigger.DOUBLECLICK) && action.getId() != null) {
                     if (context == null) {
                         context = new ActionContext(this.viewer, action.getTrigger(), null, rendering);
                     }
                     // initial draft: look up in IAction library must happen here! (TODO)
-                    new CollapseExpandAction().execute(context);
+                    config = new CollapseExpandAction().execute(context);
                 }
             }
 
             LightDiagramServices.getInstance().layoutDiagram(
-                    DiagramViewManager.getInstance().getView(
-                            viewer.getContextViewer().getViewPartId()), viewer, true, true, null);
+                    viewer.getContextViewer().getCurrentViewContext(), true, true,
+                    config != null ? ImmutableList.of(config) : null);
             
             KlighdStatusState state = new KlighdStatusState(KlighdStatusState.Status.UPDATE, viewer
                     .getContextViewer().getViewPartId(), viewer.getContextViewer()
