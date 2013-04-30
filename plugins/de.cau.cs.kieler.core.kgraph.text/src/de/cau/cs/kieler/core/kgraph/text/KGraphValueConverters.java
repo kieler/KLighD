a6784d8388c9961af050bb622dc4b47d6b7ebde9
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2013-04-30 16:34:54 +0200

    kgraph.text: - Fixed: PropertyValues are now properly supported.

--- plugins/de.cau.cs.kieler.core.kgraph.text/src/de/cau/cs/kieler/core/kgraph/text/KGraphValueConverters.java
+++ plugins/de.cau.cs.kieler.core.kgraph.text/src/de/cau/cs/kieler/core/kgraph/text/KGraphValueConverters.java
@@ -13,6 +13,9 @@
  */
 package de.cau.cs.kieler.core.kgraph.text;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.eclipse.xtext.conversion.IValueConverter;
 import org.eclipse.xtext.conversion.ValueConverter;
 import org.eclipse.xtext.conversion.ValueConverterException;
@@ -33,125 +36,178 @@ import com.google.inject.Inject;
  */
 public class KGraphValueConverters extends AbstractDeclarativeValueConverterService {
 
-	@Inject
-	private AbstractIDValueConverter idValueConverter;
+    @Inject
+    private AbstractIDValueConverter idValueConverter;
 
-	@ValueConverter(rule = "ID")
-	public IValueConverter<String> ID() {
-		return idValueConverter;
-	}
+    @ValueConverter(rule = "ID")
+    public IValueConverter<String> ID() {
+        return idValueConverter;
+    }
 
-	@Inject
-	private STRINGValueConverter stringValueConverter;
-	
-	@ValueConverter(rule = "STRING")
-	public IValueConverter<String> STRING() {
-		return stringValueConverter;
-	}
+    @Inject
+    private STRINGValueConverter stringValueConverter;
+    
+    @ValueConverter(rule = "STRING")
+    public IValueConverter<String> STRING() {
+        return stringValueConverter;
+    }
+    
+    private PropertyValueConverter propertyValueConverter = new PropertyValueConverter();
+    
+    @ValueConverter(rule = "PropertyValue")
+    public IValueConverter<String> propertyValue() {
+        return propertyValueConverter;
+    }
 
-	@Inject
-	private INTValueConverter intValueConverter;
-	
-	@ValueConverter(rule = "NATURAL")
-	public IValueConverter<Integer> NATURAL() {
-		return intValueConverter;
-	}
-	
-	@ValueConverter(rule = "RED")
-	public IValueConverter<Integer> RED() {
-		return new IntUnitSuffixConverter("r");
-	}
-	
-	@ValueConverter(rule = "GREEN")
-	public IValueConverter<Integer> GREEN() {
-		return new IntUnitSuffixConverter("g");
-	}
-	
-	@ValueConverter(rule = "BLUE")
-	public IValueConverter<Integer> BLUE() {
-		return new IntUnitSuffixConverter("b");
-	}
-	
-	@ValueConverter(rule = "ALPHA")
-	public IValueConverter<Integer> ALPHA() {
-		return new IntUnitSuffixConverter("a");
-	}
-	
-	@ValueConverter(rule = "FSIZE")
-	public IValueConverter<Integer> FSIZE() {
-		return new IntUnitSuffixConverter("pt");
-	}
-	
-	@ValueConverter(rule = "DEGREES")
-	public IValueConverter<Float> DEGREES() {
-		return new FloatUnitSuffixConverter(1, "deg");
-	}
-	
-	@ValueConverter(rule = "PERCENT")
-	public IValueConverter<Float> PERCENT() {
-		return new FloatUnitSuffixConverter(100, "%");
-	}
-	
-	private class IntUnitSuffixConverter extends AbstractNullSafeConverter<Integer> {
-		
-		private String suffix;
-		
-		IntUnitSuffixConverter(final String suffix) {
-			this.suffix = suffix;
-		}
-		
-		@Override
-		protected String internalToString(final Integer value) {
-			return intValueConverter.toString(value) + suffix;
-		}
-		
-		@Override
-		protected Integer internalToValue(final String string, final INode node)
-				throws ValueConverterException {
-			int i = (string == null) ? -1 : string.indexOf(suffix);
-			if (i >= 0) {
-				return intValueConverter.toValue(string.substring(0, i), node);
-			}
-			return intValueConverter.toValue(string, node);
-		}
-	}
-	
-	private class FloatUnitSuffixConverter extends AbstractNullSafeConverter<Float> {
-		
-		private float factor;
-		private String suffix;
-		
-		FloatUnitSuffixConverter(final float factor, final String suffix) {
-			this.factor = factor;
-			this.suffix = suffix;
-		}
-		
-		@Override
-		protected String internalToString(final Float value) {
-			if (value == null) {
-				throw new ValueConverterException("Float value may not be null.", null, null);
-			}
-			return Float.toString(value * factor) + suffix;
-		}
-		
-		@Override
-		protected Float internalToValue(final String string, final INode node)
-				throws ValueConverterException {
-			if (Strings.isEmpty(string)) {
-				throw new ValueConverterException("Couldn't convert empty string to a float value.",
-						node, null);
-			}
-			int i = string.indexOf(suffix);
-			try {
-				if (i >= 0) {
-					return Float.parseFloat(string.substring(0, i)) / factor;
-				}
-				return Float.parseFloat(string) / factor;
-			} catch (NumberFormatException e) {
-				throw new ValueConverterException("Couldn't convert '" + string + "' to a float value.",
-						node, e);
-			}
-		}
-	}
-	
+    @Inject
+    private INTValueConverter intValueConverter;
+    
+    @ValueConverter(rule = "NATURAL")
+    public IValueConverter<Integer> NATURAL() {
+        return intValueConverter;
+    }
+    
+    @ValueConverter(rule = "RED")
+    public IValueConverter<Integer> RED() {
+        return new IntUnitSuffixConverter("r");
+    }
+    
+    @ValueConverter(rule = "GREEN")
+    public IValueConverter<Integer> GREEN() {
+        return new IntUnitSuffixConverter("g");
+    }
+    
+    @ValueConverter(rule = "BLUE")
+    public IValueConverter<Integer> BLUE() {
+        return new IntUnitSuffixConverter("b");
+    }
+    
+    @ValueConverter(rule = "ALPHA")
+    public IValueConverter<Integer> ALPHA() {
+        return new IntUnitSuffixConverter("a");
+    }
+    
+    @ValueConverter(rule = "FSIZE")
+    public IValueConverter<Integer> FSIZE() {
+        return new IntUnitSuffixConverter("pt");
+    }
+    
+    @ValueConverter(rule = "DEGREES")
+    public IValueConverter<Float> DEGREES() {
+        return new FloatUnitSuffixConverter(1, "deg");
+    }
+    
+    @ValueConverter(rule = "PERCENT")
+    public IValueConverter<Float> PERCENT() {
+        return new FloatUnitSuffixConverter(100, "%");
+    }
+    
+    private class IntUnitSuffixConverter extends AbstractNullSafeConverter<Integer> {
+        private String suffix;
+        
+        IntUnitSuffixConverter(final String suffix) {
+            this.suffix = suffix;
+        }
+        
+        @Override
+        protected String internalToString(final Integer value) {
+            return intValueConverter.toString(value) + suffix;
+        }
+        
+        @Override
+        protected Integer internalToValue(final String string, final INode node)
+                throws ValueConverterException {
+            int i = (string == null) ? -1 : string.indexOf(suffix);
+            if (i >= 0) {
+                return intValueConverter.toValue(string.substring(0, i), node);
+            }
+            return intValueConverter.toValue(string, node);
+        }
+    }
+    
+    private class FloatUnitSuffixConverter extends AbstractNullSafeConverter<Float> {
+        private float factor;
+        private String suffix;
+        
+        FloatUnitSuffixConverter(final float factor, final String suffix) {
+            this.factor = factor;
+            this.suffix = suffix;
+        }
+        
+        @Override
+        protected String internalToString(final Float value) {
+            if (value == null) {
+                throw new ValueConverterException("Float value may not be null.", null, null);
+            }
+            return Float.toString(value * factor) + suffix;
+        }
+        
+        @Override
+        protected Float internalToValue(final String string, final INode node)
+                throws ValueConverterException {
+            if (Strings.isEmpty(string)) {
+                throw new ValueConverterException("Couldn't convert empty string to a float value.",
+                        node, null);
+            }
+            int i = string.indexOf(suffix);
+            try {
+                if (i >= 0) {
+                    return Float.parseFloat(string.substring(0, i)) / factor;
+                }
+                return Float.parseFloat(string) / factor;
+            } catch (NumberFormatException e) {
+                throw new ValueConverterException("Couldn't convert '" + string + "' to a float value.",
+                        node, e);
+            }
+        }
+    }
+    
+    /**
+     * Value converter for {@code PropertyValue} instances. To convert a value to a string, the value is
+     * surrounded by quotation marks if (and only if) the value cannot be parsed as a Float and is not
+     * an element of the language induced by the {@code QualifiedName} rule. To convert a string to a
+     * value, surrounding quotation marks are removed if there are any.
+     *  
+     * @author cds
+     */
+    private class PropertyValueConverter extends AbstractNullSafeConverter<String> {
+        /**
+         * Regular expression pattern that matches instances of the QualifiedName rule.
+         */
+        Pattern qualifiedNamePattern = Pattern.compile(
+                "[a-zA-Z_][a-zA-Z0-9_]*(\\.[a-zA-Z_][a-zA-Z0-9_]*)*");
+        
+        @Override
+        protected String internalToString(String value) {
+            // Check if the value can be parsed as a Float
+            try {
+                Float.parseFloat(value);
+                return value;
+            } catch (NumberFormatException e) {
+                // Simply resume execution
+            }
+            
+            // Check if the value is an instance of the QualifiedName rule
+            Matcher matcher = qualifiedNamePattern.matcher(value);
+            if (matcher.matches()) {
+                // No need to escape anything since qualified names are simple
+                return value;
+            }
+            
+            // It's a String; surround with quotation marks
+            return '"' + Strings.convertToJavaString(value, true) + '"';
+        }
+
+        @Override
+        protected String internalToValue(String string, INode node) throws ValueConverterException {
+            // Strip leading and trailing quotation mark, if any (this can be simplified if we assume
+            // that the string passed to the method either has both, a leading and a trailing quotation
+            // mark, or none; but even though we should be able to assume that, we decide to program
+            // defensively)
+            int begin = string.startsWith("\"") ? 1 : 0;
+            int end = string.length() - (string.endsWith("\"") ? 1 : 0);
+            
+            return Strings.convertFromJavaString(string.substring(begin, end), true);
+        }
+    }
 }
