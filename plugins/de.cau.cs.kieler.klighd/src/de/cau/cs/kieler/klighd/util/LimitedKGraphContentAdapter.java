df93e5f34168fbb10f848ec1ff083dcb3c80bd33
Christian Schneider <chsch@informatik.uni-kiel.de>
2013-06-24 19:14:09 +0200

    klighd+klighd.piccolo: introduced new ExpansionAwareLayoutOption, added new related convenience methods in AbstractDiagramSynthesis, restructured packages in klighd bundle, updated import statements in various classes, improved LimitedKGraphContentAdapter, also improved GraphController in klighd.piccolo

--- plugins/de.cau.cs.kieler.klighd/src/de/cau/cs/kieler/klighd/util/LimitedKGraphContentAdapter.java
+++ plugins/de.cau.cs.kieler.klighd/src/de/cau/cs/kieler/klighd/util/LimitedKGraphContentAdapter.java
@@ -14,18 +14,40 @@
 package de.cau.cs.kieler.klighd.util;
 
 import java.util.Iterator;
+import java.util.List;
 
+import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.util.EContentAdapter;
 import org.eclipse.emf.ecore.util.InternalEList;
 
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Iterators;
+import com.google.common.collect.Lists;
 
 import de.cau.cs.kieler.core.kgraph.KGraphData;
-import de.cau.cs.kieler.core.kgraph.KNode;
+import de.cau.cs.kieler.core.kgraph.KGraphElement;
 
 /**
+ * A specialized EContentAdapter that attaches itself only to children of a certain type. This type
+ * is set in the constructor. Once attached to such a child it attaches itself recursively to all
+ * child's children.<br>
+ * <br>
+ * It is used to observe changes in {@link de.cau.cs.kieler.kiml.klayoutdata.KShapeLayout
+ * KShapeLayouts} and {@link de.cau.cs.kieler.kiml.klayoutdata.KEdgeLayout KEdgeLayouts} of
+ * {@link KGraphElement KGraphElements}, for example. The special benefit of this content adapter
+ * type is the tolerance of complete exchanges of such objects, which happens e.g. while
+ * incorporating EMF Compare.<br>
+ * <br>
+ * Thus, the adapter is to be attached to the related {@link KGraphElement} and adapts itself to the
+ * children of the intended type if they are already present or added later on. It can, for
+ * instance, also be used to observe changes on {@link de.cau.cs.kieler.core.krendering.KRendering
+ * KRendering} objects only.
+ * 
  * @author chsch
  */
 public class LimitedKGraphContentAdapter extends EContentAdapter {
@@ -42,17 +64,20 @@ public class LimitedKGraphContentAdapter extends EContentAdapter {
     }
 
     /**
-     * {@inheritDoc}
+     * {@inheritDoc}<br>
+     * <br>
+     * This specialization handles the initialization case, i.e. the recursive attachment while
+     * adding it to a {@link KGraphElement}.
      */
     protected void setTarget(final EObject target) {
-        if (target instanceof KNode) {
+        if (target instanceof KGraphElement) {
+            basicSetTarget(target);
+            
             // check if a shape layout is exists
-            KNode node = (KNode) target;
-            KGraphData layoutData = node.getData(this.layoutDataClass);
+            KGraphData layoutData = ((KGraphElement) target).getData(this.layoutDataClass);
 
             if (layoutData != null) {
                 // add the adapter to all contents of the shape layout
-                basicSetTarget(target);
                 Iterator<? extends Notifier> i = resolve() ? layoutData.eContents().iterator()
                         : ((InternalEList<? extends Notifier>) layoutData.eContents())
                                 .basicIterator();
@@ -64,6 +89,53 @@ public class LimitedKGraphContentAdapter extends EContentAdapter {
             }
         } else {
             super.setTarget(target);
+        }
+    }
+
+    /**
+     * {@inheritDoc}<br>
+     * <br>
+     * This specialization handles the update case, i.e. the recursive attachment and removal after
+     * elements have been (deeply) added or removed to the root target after the adapter has been
+     * added.
+     */
+    protected void handleContainment(final Notification n) {
+        if (getTarget().equals(n.getNotifier())) {
+            switch (n.getEventType()) {
+            case Notification.ADD:
+                if (n.getNewValue() != null && n.getNewValue().getClass().equals(this.layoutDataClass)) {
+                    super.handleContainment(n);
+                }
+                break;
+                
+            case Notification.ADD_MANY:                
+                @SuppressWarnings("unchecked")
+                List<KGraphData> filtered =
+                        Lists.newArrayList(Iterables.filter((Iterable<Notifier>) n.getNewValue(),
+                                this.layoutDataClass));
+                if (!filtered.isEmpty()) {
+                    super.handleContainment(new ENotificationImpl(
+                            (InternalEObject) n.getNotifier(), n.getEventType(),
+                            (EStructuralFeature) n.getFeature(), n.getOldValue(), filtered, n
+                                    .getPosition()));
+                }
+                break;
+                
+            case Notification.REMOVE:
+            case Notification.REMOVE_MANY:
+                super.handleContainment(n);
+                break;
+                
+            case Notification.RESOLVE:
+            case Notification.UNSET:
+            case Notification.SET:
+                // TODO
+                // I think the support of these cases doesn't work similarly easy as above since addition
+                //  and removal are mixed in the super method. Luckily they are not required, yet, since
+                //  KGraphElement has only a collection field.
+            }            
+        } else {
+            super.handleContainment(n);
         }
     }
 }
