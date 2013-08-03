0fd7fd38f681b586a0538a593b105f6b414c350b
Christian Schneider <chsch@informatik.uni-kiel.de>
2013-08-03 20:33:08 +0200

    klighd.piccolo: minor documentation improvement in KlighdPath

--- plugins/de.cau.cs.kieler.klighd.piccolo/src/de/cau/cs/kieler/klighd/piccolo/internal/nodes/KlighdPath.java
+++ plugins/de.cau.cs.kieler.klighd.piccolo/src/de/cau/cs/kieler/klighd/piccolo/internal/nodes/KlighdPath.java
@@ -58,8 +58,7 @@ import edu.umd.cs.piccolo.util.PPickPath;
  * {@link #addChild(PNode)}/{@link #removeChild(PNode)} in case of rendering changes, by
  * {@link #setBounds(double, double, double, double)} in case of layout changes, and in case of pure
  * style changes by the {@link de.cau.cs.kieler.core.kgraph.KGraphElement KGraphElement} rendering
- * controllers (
- * {@link de.cau.cs.kieler.klighd.piccolo.internal.controller.AbstractKGERenderingController
+ * controllers ({@link de.cau.cs.kieler.klighd.piccolo.internal.controller.AbstractKGERenderingController
  * #updateStyles() AbstractKGERenderingController#updateStyles()}) after all rendering and style
  * changes are performed.
  * 
@@ -514,7 +513,7 @@ public class KlighdPath extends PNode {
             final double height) {
         // unused (left it the for the case it might be helpful in future,
         //  should not sink into obscurity) 
-        //  if it is used again one make sure that no recursive cycles occur with
+        // if it is used again one have to make sure that no recursive cycles occur with
         //  #updateBoundsFromPath
     }
 
