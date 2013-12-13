9354808ae2cbf7d6c26ea5d16dc761d0acc14de3
Christian Schneider <chsch@informatik.uni-kiel.de>
2013-12-13 15:52:10 +0100

    klighd.ui: contributed new PiccoloViewerProvider, add required registration of viewer provider & preference page

---
+++ plugins/de.cau.cs.kieler.klighd.ui/src/de/cau/cs/kieler/klighd/ui/internal/viewers/PiccoloViewerProvider.java
@@ -0,0 +1,95 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2011 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.klighd.ui.internal.viewers;
+
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
+
+import de.cau.cs.kieler.core.kgraph.KNode;
+import de.cau.cs.kieler.klighd.IViewer;
+import de.cau.cs.kieler.klighd.IViewerProvider;
+import de.cau.cs.kieler.klighd.piccolo.viewer.PiccoloOutlinePage;
+import de.cau.cs.kieler.klighd.piccolo.viewer.PiccoloViewer;
+import de.cau.cs.kieler.klighd.viewers.ContextViewer;
+
+/**
+ * A viewer provider for the Piccolo2D viewer for KGraphs with attached KRendering data.
+ * 
+ * @author mri
+ * @author chsch
+ */
+public class PiccoloViewerProvider implements IViewerProvider<KNode> {
+
+    /** the identifier for this viewer provider as specified in the extension point. */
+    public static final String ID = "de.cau.cs.kieler.klighd.piccolo.piccoloViewer";
+
+    /**
+     * {@inheritDoc}
+     */
+    public Class<KNode> getModelClass() {
+        return KNode.class;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public IViewer<KNode> createViewer(final ContextViewer parentViewer, final Composite parent) {
+        return new PiccoloViewer(parentViewer, parent) {
+
+            @Override
+            protected PiccoloOutlinePage createDiagramOutlinePage() {
+                return new PiccoloContentOutlinePage();
+            }
+        };
+    }
+
+    /**
+     * A. 
+     * @author chsch
+     */
+    private static class PiccoloContentOutlinePage extends PiccoloOutlinePage implements
+            IContentOutlinePage {
+
+        /**
+         * {@inheritDoc}
+         */
+        public void addSelectionChangedListener(final ISelectionChangedListener listener) {
+            // selection is not supported by this outline page
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public void removeSelectionChangedListener(final ISelectionChangedListener listener) {
+            // selection is not supported by this outline page
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public ISelection getSelection() {
+            // selection is not supported by this outline page
+            return null;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public void setSelection(final ISelection selection) {
+            // selection is not supported by this outline page
+        }
+    }
+}
