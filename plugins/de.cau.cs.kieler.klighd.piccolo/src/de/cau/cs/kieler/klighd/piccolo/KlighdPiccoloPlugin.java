fc09f46783b8997e0c07875c0af780b1be2a462b
Christian Schneider <chsch@informatik.uni-kiel.de>
2013-12-13 18:27:53 +0100

    klighd dependent: consolidated dependencies, re-exported them if reasonable

--- plugins/de.cau.cs.kieler.klighd.piccolo/src/de/cau/cs/kieler/klighd/piccolo/KlighdPiccoloPlugin.java
+++ plugins/de.cau.cs.kieler.klighd.piccolo/src/de/cau/cs/kieler/klighd/piccolo/KlighdPiccoloPlugin.java
@@ -13,7 +13,6 @@
  */
 package de.cau.cs.kieler.klighd.piccolo;
 
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -62,17 +61,4 @@ public class KlighdPiccoloPlugin extends AbstractUIPlugin {
     public static KlighdPiccoloPlugin getDefault() {
         return plugin;
     }
-
-    /**
-     * Returns an image descriptor for the image file at the given plug-in
-     * relative path.
-     * 
-     * @param path
-     *            the path
-     * @return the image descriptor
-     */
-    public static ImageDescriptor getImageDescriptor(final String path) {
-        return imageDescriptorFromPlugin(PLUGIN_ID, path);
-    }
-
 }
