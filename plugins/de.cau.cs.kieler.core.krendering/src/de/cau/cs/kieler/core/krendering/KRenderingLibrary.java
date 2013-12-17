2902b1fefbf7b2a998f421719600206f1a0cf2b1
Christian Schneider <chsch@informatik.uni-kiel.de>
2013-12-17 14:51:15 +0100

    core.krendering: added further 'set[Target]Color2(...)' methods in KColoring; improved documentation of KRenderingLibrary

--- plugins/de.cau.cs.kieler.core.krendering/src/de/cau/cs/kieler/core/krendering/KRenderingLibrary.java
+++ plugins/de.cau.cs.kieler.core.krendering/src/de/cau/cs/kieler/core/krendering/KRenderingLibrary.java
@@ -1,20 +1,7 @@
-/**
- * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
- * 
- * http://www.informatik.uni-kiel.de/rtsys/kieler/
- * 
- * Copyright 2012 by
- * + Christian-Albrechts-University of Kiel
- *   + Department of Computer Science
- *     + Real-Time and Embedded Systems Group
- * 
- * This code is provided under the terms of the Eclipse Public License (EPL).
- * See the file epl-v10.html for the license text.
- */
 package de.cau.cs.kieler.core.krendering;
 
-import de.cau.cs.kieler.core.kgraph.KGraphData;
 
+import de.cau.cs.kieler.core.kgraph.KGraphData;
 import org.eclipse.emf.common.util.EList;
 
 /**
@@ -23,7 +10,12 @@ import org.eclipse.emf.common.util.EList;
  * <!-- end-user-doc -->
  *
  * <!-- begin-model-doc -->
- * contains all the renderings 
+ * <br><br>
+ * Instances of this class may be employed in {@link de.cau.cs.kieler.core.kgraph.KGraphElement KGraphElements} for accommodating
+ * {@link KRendering KRenderings} that are shared by multiple other {@link de.cau.cs.kieler.core.kgraph.KGraphElement KGraphElements}
+ * and referenced by means of {@link KRenderingRef KRenderingRefs}.<br>
+ * {@link KRenderingRef KRenderingRefs}, however, may reference any arbitrary {@link KRendering} its deposition in a KRenderingLibrary is
+ * not required. Thus, KRenderingLibraries are just for structuring view models, they don't contribute any semantics.
  * <!-- end-model-doc -->
  *
  * <p>
@@ -42,10 +34,6 @@ public interface KRenderingLibrary extends KGraphData {
      * Returns the value of the '<em><b>Renderings</b></em>' containment reference list.
      * The list contents are of type {@link de.cau.cs.kieler.core.krendering.KStyleHolder}.
      * <!-- begin-user-doc -->
-     * <p>
-     * If the meaning of the '<em>Renderings</em>' containment reference list isn't clear,
-     * there really should be more of a description here...
-     * </p>
      * <!-- end-user-doc -->
      * <!-- begin-model-doc -->
      * The renderings defined by this library.
--- plugins/de.cau.cs.kieler.core.krendering/src/de/cau/cs/kieler/core/krendering/impl/KRenderingPackageImpl.java
+++ plugins/de.cau.cs.kieler.core.krendering/src/de/cau/cs/kieler/core/krendering/impl/KRenderingPackageImpl.java
@@ -2580,6 +2580,17 @@ public class KRenderingPackageImpl extends EPackageImpl implements KRenderingPac
         g1 = createEGenericType(kColoringEClass_T);
         initEOperation(op, g1);
 
+        op = addEOperation(kColoringEClass, null, "setColor2", 0, 1, IS_UNIQUE, IS_ORDERED);
+        addEParameter(op, this.getKColor(), "color", 0, 1, IS_UNIQUE, IS_ORDERED);
+        g1 = createEGenericType(kColoringEClass_T);
+        initEOperation(op, g1);
+
+        op = addEOperation(kColoringEClass, null, "setColor2", 0, 1, IS_UNIQUE, IS_ORDERED);
+        addEParameter(op, this.getKColor(), "color", 0, 1, IS_UNIQUE, IS_ORDERED);
+        addEParameter(op, ecorePackage.getEInt(), "alpha", 0, 1, IS_UNIQUE, IS_ORDERED);
+        g1 = createEGenericType(kColoringEClass_T);
+        initEOperation(op, g1);
+
         op = addEOperation(kColoringEClass, null, "setTargetColor", 0, 1, IS_UNIQUE, IS_ORDERED);
         addEParameter(op, ecorePackage.getEInt(), "red", 0, 1, IS_UNIQUE, IS_ORDERED);
         addEParameter(op, ecorePackage.getEInt(), "green", 0, 1, IS_UNIQUE, IS_ORDERED);
@@ -2603,6 +2614,17 @@ public class KRenderingPackageImpl extends EPackageImpl implements KRenderingPac
         op = addEOperation(kColoringEClass, null, "setTargetColor", 0, 1, IS_UNIQUE, IS_ORDERED);
         addEParameter(op, this.getColors(), "color", 0, 1, IS_UNIQUE, IS_ORDERED);
         addEParameter(op, ecorePackage.getEInt(), "alpha", 0, 1, IS_UNIQUE, IS_ORDERED);
+        g1 = createEGenericType(kColoringEClass_T);
+        initEOperation(op, g1);
+
+        op = addEOperation(kColoringEClass, null, "setTargetColor2", 0, 1, IS_UNIQUE, IS_ORDERED);
+        addEParameter(op, this.getKColor(), "targetColor", 0, 1, IS_UNIQUE, IS_ORDERED);
+        g1 = createEGenericType(kColoringEClass_T);
+        initEOperation(op, g1);
+
+        op = addEOperation(kColoringEClass, null, "setTargetColor2", 0, 1, IS_UNIQUE, IS_ORDERED);
+        addEParameter(op, this.getKColor(), "targetColor", 0, 1, IS_UNIQUE, IS_ORDERED);
+        addEParameter(op, ecorePackage.getEInt(), "targetAlpha", 0, 1, IS_UNIQUE, IS_ORDERED);
         g1 = createEGenericType(kColoringEClass_T);
         initEOperation(op, g1);
 
