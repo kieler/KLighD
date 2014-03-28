af652d5fae2d68ab6d8f37e0dba7b66a12441b2f
Christian Schneider <chsch@informatik.uni-kiel.de>
2014-03-28 09:56:26 +0100

    klighd.ui: contributed widgets in KlighdPreferencePage for configuring the magnification lens settings

--- plugins/de.cau.cs.kieler.klighd.ui/src/de/cau/cs/kieler/klighd/ui/internal/KlighdPreferencePage.java
+++ plugins/de.cau.cs.kieler.klighd.ui/src/de/cau/cs/kieler/klighd/ui/internal/KlighdPreferencePage.java
@@ -24,6 +24,8 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Spinner;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
@@ -36,6 +38,7 @@ import de.cau.cs.kieler.klighd.internal.Messages;
  * Preference page for KLighD.
  * 
  * @author cds
+ * @author chsch
  */
 public final class KlighdPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
 
@@ -50,7 +53,11 @@ public final class KlighdPreferencePage extends PreferencePage implements IWorkb
     private Button zoomToFocus;
     /** radio button for no zoom. */
     private Button zoomNone;
-
+    
+    private Spinner magLensWidth;
+    private Spinner magLensHeight;
+    private Spinner magLensScale;
+    
     /**
      * Creates a new instance.
      */
@@ -76,6 +83,13 @@ public final class KlighdPreferencePage extends PreferencePage implements IWorkb
                 .setValue(KlighdPreferences.ANIMATE_LAYOUT, animationCheckBox.getSelection());
         ZoomStyle zoomStyle = getZoomStyleFromSelection();
         preferenceStore.setValue(KlighdPreferences.ZOOM_STYLE, zoomStyle.name());
+        
+        preferenceStore.setValue(KlighdPreferences.MAGNIFICATION_LENS_WIDTH,
+                magLensWidth.getSelection());
+        preferenceStore.setValue(KlighdPreferences.MAGNIFICATION_LENS_HEIGHT,
+                magLensHeight.getSelection());
+        preferenceStore.setValue(KlighdPreferences.MAGNIFICATION_LENS_SCALE,
+                magLensScale.getSelection());
 
         return true;
     }
@@ -91,6 +105,14 @@ public final class KlighdPreferencePage extends PreferencePage implements IWorkb
                 .getDefaultBoolean(KlighdPreferences.ANIMATE_LAYOUT));
         setZoomStyleSelection(ZoomStyle.valueOf(preferenceStore
                 .getDefaultString(KlighdPreferences.ZOOM_STYLE)));
+        
+        magLensWidth.setSelection(preferenceStore
+                .getDefaultInt(KlighdPreferences.MAGNIFICATION_LENS_WIDTH));
+        magLensHeight.setSelection(preferenceStore
+                .getDefaultInt(KlighdPreferences.MAGNIFICATION_LENS_HEIGHT));
+        magLensScale.setSelection(preferenceStore
+                .getDefaultInt(KlighdPreferences.MAGNIFICATION_LENS_SCALE));
+
     }
 
     // /////////////////////////////////////////////////////////////////////////////////////////
@@ -116,6 +138,9 @@ public final class KlighdPreferencePage extends PreferencePage implements IWorkb
 
         Group zoomGroup = createZoomToFitGroup(composite);
         zoomGroup.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+        
+        Group magnificationLensGroup = createMagnificationLensConfig(composite);
+        magnificationLensGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
         
         return composite;
     }
@@ -174,7 +199,7 @@ public final class KlighdPreferencePage extends PreferencePage implements IWorkb
         
         return zoomStyleGroup;
     }
-
+    
     // End of UI code -- no magic numbers allowed anymore.
     // CHECKSTYLEON MagicNumber
 
@@ -199,5 +224,46 @@ public final class KlighdPreferencePage extends PreferencePage implements IWorkb
         } else {
             return ZoomStyle.NONE;
         }
+    }
+    
+    private Group createMagnificationLensConfig(final Composite parent) {
+        // SUPPRESS CHECKSTYLE NEXT 35 MagicNumber
+        
+        final Group magnificationLensGroup = new Group(parent, SWT.NONE);
+        magnificationLensGroup.setText("Magification lens");
+        magnificationLensGroup.setLayout(new GridLayout(6, false));
+        
+        final Label width = new Label(magnificationLensGroup, SWT.NONE);
+        width.setText("width:");
+        magLensWidth = new Spinner(magnificationLensGroup, SWT.NONE);
+        magLensWidth.setMinimum(100);
+        magLensWidth.setMaximum(1000);
+        magLensWidth.setIncrement(10);
+        magLensWidth.setPageIncrement(50);
+        magLensWidth.setSelection(getPreferenceStore().getInt(
+                KlighdPreferences.MAGNIFICATION_LENS_WIDTH));
+        
+        final Label height = new Label(magnificationLensGroup, SWT.NONE);
+        height.setText("height:");
+        magLensHeight = new Spinner(magnificationLensGroup, SWT.NONE);
+        magLensHeight.setMinimum(100);
+        magLensHeight.setMaximum(1000);
+        magLensHeight.setIncrement(10);
+        magLensHeight.setPageIncrement(50);
+        magLensHeight.setSelection(getPreferenceStore().getInt(
+                KlighdPreferences.MAGNIFICATION_LENS_HEIGHT));
+        
+        final Label scale = new Label(magnificationLensGroup, SWT.NONE);
+        scale.setText("scale:");
+        magLensScale = new Spinner(magnificationLensGroup, SWT.NONE);
+        magLensScale.setMinimum(50);
+        magLensScale.setDigits(2);
+        magLensScale.setMaximum(500);
+        magLensScale.setIncrement(25);
+        magLensScale.setPageIncrement(100);
+        magLensScale.setSelection(getPreferenceStore().getInt(
+                KlighdPreferences.MAGNIFICATION_LENS_SCALE));
+        
+        return magnificationLensGroup;
     }
 }
