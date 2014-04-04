8a821c81a23ce35e2de70780036a8e4c9db43af4
Christian Schneider <chsch@informatik.uni-kiel.de>
2014-04-04 23:36:38 +0200

    klighd.piccolo: moved helper methods in DiagramController to dedicated DiagramControllerHelper class in order to reduce DiagramController's size

---
+++ plugins/de.cau.cs.kieler.klighd.piccolo/src/de/cau/cs/kieler/klighd/piccolo/internal/controller/DiagramControllerHelper.java
@@ -0,0 +1,280 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2014 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.klighd.piccolo.internal.controller;
+
+import java.awt.geom.Point2D;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+
+import de.cau.cs.kieler.core.kgraph.KEdge;
+import de.cau.cs.kieler.core.kgraph.KGraphElement;
+import de.cau.cs.kieler.core.kgraph.KLabel;
+import de.cau.cs.kieler.core.kgraph.KNode;
+import de.cau.cs.kieler.core.kgraph.KPort;
+import de.cau.cs.kieler.core.kgraph.util.KGraphSwitch;
+import de.cau.cs.kieler.klighd.piccolo.internal.nodes.INode;
+import de.cau.cs.kieler.klighd.piccolo.internal.nodes.KChildAreaNode;
+import de.cau.cs.kieler.klighd.piccolo.internal.nodes.KEdgeNode;
+import de.cau.cs.kieler.klighd.piccolo.internal.util.NodeUtil;
+import de.cau.cs.kieler.klighd.util.ModelingUtil;
+import de.cau.cs.kieler.klighd.util.RenderingContextData;
+import edu.umd.cs.piccolo.PNode;
+
+/**
+ * Helper class introduced in order to reduce the size of {@link DiagramController}.
+ * 
+ * @author chsch
+ */
+public final class DiagramControllerHelper {
+
+    /** the attribute key for the edge offset listeners. */
+    private static final Object EDGE_OFFSET_LISTENER_KEY = new Object();
+
+    /** the attribute key for the nodes listed by edge offset listeners. */
+    private static final Object EDGE_OFFSET_LISTENED_KEY = new Object();
+
+    /**
+     * Standard hidden constructor.
+     */
+    private DiagramControllerHelper() {
+    }
+
+    /**
+     * Finds the parent node for the edge representation and adds the edge to that node
+     * representations child area. This is needed since the clipping property of
+     * {@link KChildAreaNode}s will clip the edges. Hence they are located in the
+     * {@link KChildAreaNode} of lowest common ancestor.
+     * 
+     * @param edgeRep
+     *            the edge representation
+     */
+    static void updateEdgeParent(final KEdgeNode edgeRep) {
+        KEdge edge = edgeRep.getGraphElement();
+        KNode source = edge.getSource();
+        KNode target = edge.getTarget();
+        if (source != null && target != null) {
+            KNode commonParent = findLowestCommonAncestor(source, target);
+            INode commonParentNode =
+                    RenderingContextData.get(commonParent).getProperty(DiagramController.REP);
+            if (commonParentNode != null) {
+                KChildAreaNode childAreaNode = commonParentNode.getChildAreaNode();
+                childAreaNode.addEdge(edgeRep);
+            }
+        }
+    }
+
+    /**
+     * Updates the offset of the edge representation. Takes care about insets due to
+     * {@link de.cau.cs.kieler.core.krendering.KPlacementData KPlacementData} and the relocation
+     * performed in {@link #updateEdgeParent(KEdgeNode)}-
+     * 
+     * @param edgeNode
+     *            the edge representation
+     */
+    static void updateEdgeOffset(final KEdgeNode edgeNode) {
+        final KChildAreaNode edgeNodeParent = edgeNode.getParentChildArea();
+        if (edgeNodeParent != null) {
+            KEdge edge = edgeNode.getGraphElement();
+            // chsch: change due to KIELER-1988; // SUPPRESS CHECKSTYLE NEXT 3 LineLength
+            // edges uses different reference points as indicated by
+            // http://rtsys.informatik.uni-kiel.de/~kieler/files/documentation/klayoutdata-reference-points.png
+            // see page http://rtsys.informatik.uni-kiel.de/confluence/display/KIELER/KLayoutData+Meta+Model
+            INode sourceParentNode = RenderingContextData.get(determineReferenceNodeOf(edge))
+                    .getProperty(DiagramController.REP);
+            final KChildAreaNode relativeChildArea = sourceParentNode.getChildAreaNode();
+
+            // chsch: The following listener updates the offset of the edge depending the parent nodes.
+            // It is attached to all parent nodes that are part of the containment hierarchy,
+            //  i.e., KNodeNodes, KChildAreaNodes, KlighdPaths...!
+            // The listener is sensitive to changes to the 'transform' of those elements.
+            // It is important, in case of the change of a parent KNode's rendering,
+            //  that its related KChildAreaNode is contained in any other PNode!
+            PropertyChangeListener listener = new PropertyChangeListener() {
+
+                // assumption: KChildAreaNodes in the containment hierarchy do not have an empty
+                // 'parent' reference, otherwise an offset change has been performed on a
+                // non-contained
+                // child area. This must be avoided under all circumstances!
+                public void propertyChange(final PropertyChangeEvent event) {
+
+                    // calculate the offset
+                    Point2D offset = new Point2D.Double(0, 0);
+                    PNode currentNode = relativeChildArea;
+                    while (currentNode != null && currentNode != edgeNodeParent) {
+                        currentNode.localToParent(offset);
+                        currentNode = currentNode.getParent();
+                    }
+
+                    // apply the offset
+                    NodeUtil.applyTranslation(edgeNode, offset);
+                }
+            };
+
+            // remember the listener
+            edgeNode.addAttribute(EDGE_OFFSET_LISTENER_KEY, listener);
+
+            // calculate the offset and register the update offset listener
+            List<PNode> listenedNodes = Lists.newLinkedList();
+            Point2D offset = new Point2D.Double(0, 0);
+            PNode currentNode = relativeChildArea;
+            while (currentNode != null && currentNode != edgeNodeParent) {
+                currentNode.localToParent(offset);
+                currentNode.addPropertyChangeListener(PNode.PROPERTY_TRANSFORM, listener);
+                listenedNodes.add(currentNode);
+                currentNode = currentNode.getParent();
+            }
+
+            // remember the listened nodes
+            edgeNode.addAttribute(EDGE_OFFSET_LISTENED_KEY, listenedNodes);
+
+            // apply the offset
+            NodeUtil.applyTranslation(edgeNode, offset);
+        }
+    }
+
+    /**
+     * Removes all listeners used to update the edge representations offset from the associated
+     * nodes.
+     * 
+     * @param edgeNode
+     *            the edge representation
+     */
+    static void removeEdgeOffsetListener(final KEdgeNode edgeNode) {
+        PropertyChangeListener listener = (PropertyChangeListener) edgeNode
+                .getAttribute(EDGE_OFFSET_LISTENER_KEY);
+        @SuppressWarnings("unchecked")
+        List<PNode> listenedNodes = (List<PNode>) edgeNode.getAttribute(EDGE_OFFSET_LISTENED_KEY);
+        if (listener != null && listenedNodes != null) {
+            for (PNode listenedNode : listenedNodes) {
+                listenedNode.removePropertyChangeListener(PNode.PROPERTY_TRANSFORM, listener);
+            }
+        }
+        edgeNode.addAttribute(EDGE_OFFSET_LISTENER_KEY, null);
+        edgeNode.addAttribute(EDGE_OFFSET_LISTENED_KEY, null);
+    }
+
+    /**
+     * Needed as edge coordinates uses different reference nodes as indicated by
+     * http://rtsys.informatik.uni-kiel.de/~kieler/files/documentation/klayoutdata-reference-points.png
+     * see page http://rtsys.informatik.uni-kiel.de/confluence/display/KIELER/KLayoutData+Meta+Model.
+     * 
+     * @param edge
+     *            the edge whose reference node is to be determined,
+     * @return its reference node
+     */
+    private static KNode determineReferenceNodeOf(final KEdge edge) {
+        // in case of a self loop, the reference node the source/target's parent
+        if (edge.getSource() == edge.getTarget()) {
+            return edge.getSource().getParent();
+        }
+
+        // determine whether the edge directs to an inner node
+        KNode node = edge.getTarget();
+        while (node != null && node != edge.getSource()) {
+            node = node.getParent();
+        }
+        // if (node != null) holds, node == edge.getSource() holds and therefore the target node is
+        // contained in the source node; in this case the source node's child area denotes the
+        // reference point of the edge's coordinates, the child area of the source node's parent
+        // otherwise, as indicated by the above mentioned illustration
+        return node != null ? edge.getSource() : edge.getSource().getParent();
+    }
+
+    /**
+     * Returns the lowest common ancestor to both given nodes.
+     * 
+     * @param source
+     *            the first node
+     * @param target
+     *            the second node
+     * @return the lowest common ancestor
+     */
+    private static KNode findLowestCommonAncestor(final KNode source, final KNode target) {
+        if (source.getParent() == target.getParent()) {
+            // this is the case in common graphs, e.g. state charts
+            return source.getParent();
+        } else if (target.getParent() == source) {
+            // this is the case if (in data flow diagrams) the edge connects an input port
+            //  of a composite node with an inner node 
+            return source;
+        } else if (source.getParent() == target) {
+            // this is the case if (in data flow diagrams) the edge connects an inner node
+            //  of a composite node with an output port of the latter one 
+            return target;
+        } else {
+            final List<EObject> sourceParents = Lists.newArrayList(ModelingUtil.eAllContainers(source));
+            final List<EObject> targetParents = Lists.newArrayList(ModelingUtil.eAllContainers(target));
+            
+            sourceParents.retainAll(targetParents);
+            return (KNode) Iterables.getFirst(sourceParents, null);
+        }
+    }    
+
+    /**
+     * Removes all rendering context data from the given graph element and all child elements.<br>
+     * <br>
+     * Review: knodes maintain context information, these information is removed by this statement,
+     * mainly needed if textual kgraph editor is used shall be obsolete if an adequate update
+     * strategy is available
+     * 
+     * @param element
+     *            the graph element
+     */
+    static void resetGraphElement(final KGraphElement element) {
+        // remove rendering context data from the element
+        RenderingContextData data = element.getData(RenderingContextData.class);
+        if (data != null) {
+            element.getData().remove(data);
+        }
+
+        // proceed recursively with child elements
+        new KGraphSwitch<Boolean>() {
+            public Boolean caseKNode(final KNode node) {
+                for (KNode child : node.getChildren()) {
+                    resetGraphElement(child);
+                }
+                for (KEdge edge : node.getOutgoingEdges()) {
+                    resetGraphElement(edge);
+                }
+                for (KPort port : node.getPorts()) {
+                    resetGraphElement(port);
+                }
+                for (KLabel label : node.getLabels()) {
+                    resetGraphElement(label);
+                }
+                return true;
+            }
+
+            public Boolean caseKEdge(final KEdge edge) {
+                for (KLabel label : edge.getLabels()) {
+                    resetGraphElement(label);
+                }
+                return true;
+            }
+
+            public Boolean caseKPort(final KPort port) {
+                for (KLabel label : port.getLabels()) {
+                    resetGraphElement(label);
+                }
+                return true;
+            }
+        } /**/.doSwitch(element);
+    }
+}
