5382bf9e32464262316fb5f73edced3b14cb00f0
Ulf Rueegg <uru@informatik.uni-kiel.de>
2014-04-30 21:39:15 +1000

    klighd.ui: added action that lets the user save the underlying kgraph of a klighd view

---
+++ plugins/de.cau.cs.kieler.klighd.ui/src/de/cau/cs/kieler/klighd/ui/internal/handlers/KlighdSaveKGraphHandler.java
@@ -0,0 +1,118 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2014 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.klighd.ui.internal.handlers;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.dialogs.SaveAsDialog;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.statushandlers.StatusManager;
+
+import de.cau.cs.kieler.klighd.KlighdTreeSelection;
+import de.cau.cs.kieler.klighd.ViewContext;
+import de.cau.cs.kieler.klighd.ui.KlighdUIPlugin;
+
+/**
+ * Handler to store the view model KGraph of a KlighD view to a file.
+ * 
+ * @author uru
+ */
+public class KlighdSaveKGraphHandler extends AbstractHandler {
+
+    /**
+     * {@inheritDoc}
+     */
+    public Object execute(final ExecutionEvent event) throws ExecutionException {
+        final KlighdTreeSelection selection;
+
+        // in case this handler is invoked via a context menu,
+        // the activeMenuSelection (ISources#ACTIVE_MENU_SELECTION_NAME) is available
+        ISelection s = HandlerUtil.getActiveMenuSelection(event);
+
+        if (s instanceof KlighdTreeSelection) {
+            // if it's a KLighD selection (it is supposed to be, we're fine :-)
+            selection = (KlighdTreeSelection) s;
+
+        } else if (s == null) {
+            // if no activeMenuSelection is set, the handler may be called by the main menu,
+            // toolbar, or a key binding; refer to the global selection in that case
+            s = HandlerUtil.getCurrentSelectionChecked(event);
+
+            if (s instanceof KlighdTreeSelection) {
+                // again if it's a KLighD selection (it is supposed to be, we're fine :-)
+                selection = (KlighdTreeSelection) s;
+
+            } else {
+                // something really strange must have happened
+                return null;
+            }
+        } else {
+            // something really strange must have happened
+            return null;
+        }
+
+        final ViewContext viewContext = selection.getViewContext();
+        if (viewContext != null) {
+            StatusManager.getManager().handle(
+                    new Status(IStatus.ERROR, KlighdUIPlugin.PLUGIN_ID,
+                            "Could not determine ViewContext."), StatusManager.SHOW);
+        }
+
+        final Shell shell = Display.getCurrent().getActiveShell();
+        final SaveAsDialog fd = new SaveAsDialog(shell);
+
+        try {
+            // show a save as dialog
+            final int success = fd.open();
+            if (shell != null && success == SaveAsDialog.OK) {
+                // retrieve selected
+                String file = fd.getResult().toOSString().toString();
+                ResourceSet rs = new ResourceSetImpl();
+                Resource r = rs.createResource(URI.createFileURI(file));
+
+                // write a copy of the view model kgraph to the selected file
+                EObject copy = EcoreUtil.copy(viewContext.getViewModel());
+                r.getContents().add(copy);
+                r.save(Collections.emptyMap());
+
+            }
+        } catch (IOException e) {
+            StatusManager.getManager().handle(
+                    new Status(IStatus.ERROR, KlighdUIPlugin.PLUGIN_ID,
+                            "Could not write the KGraph to selected resource.", e),
+                    StatusManager.SHOW);
+        } catch (Exception e) {
+            StatusManager.getManager().handle(
+                    new Status(IStatus.ERROR, KlighdUIPlugin.PLUGIN_ID, "Error occurred.", e),
+                    StatusManager.SHOW);
+        }
+
+        return null;
+    }
+}
