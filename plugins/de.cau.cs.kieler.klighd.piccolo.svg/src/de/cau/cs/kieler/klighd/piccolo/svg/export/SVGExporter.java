ada6b8b5ec0ab64bb47835634c31d8fe9f48e364
Christian Schneider <chsch@informatik.uni-kiel.de>
2014-05-18 23:39:50 +0200

    klighd.piccolo + .svg: massively refactored BitmapExporter and SVGExporter in order to properly handle clipped diagrams, put common code into two methods in common super class KlighdCanvasExporter

--- plugins/de.cau.cs.kieler.klighd.piccolo.svg/src/de/cau/cs/kieler/klighd/piccolo/svg/export/SVGExporter.java
+++ plugins/de.cau.cs.kieler.klighd.piccolo.svg/src/de/cau/cs/kieler/klighd/piccolo/svg/export/SVGExporter.java
@@ -13,22 +13,32 @@
  */
 package de.cau.cs.kieler.klighd.piccolo.svg.export;
 
+import java.io.IOException;
 import java.io.OutputStream;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ui.statushandlers.StatusManager;
+
+import de.cau.cs.kieler.klighd.KlighdPlugin;
 import de.cau.cs.kieler.klighd.piccolo.export.KlighdCanvasExporter;
 import de.cau.cs.kieler.klighd.piccolo.internal.nodes.KlighdCanvas;
-import de.cau.cs.kieler.klighd.piccolo.svg.KlighdSVGCanvas;
-import edu.umd.cs.piccolo.PCamera;
+import de.cau.cs.kieler.klighd.piccolo.internal.nodes.KlighdMainCamera;
+import de.cau.cs.kieler.klighd.piccolo.svg.KlighdAbstractSVGGraphics;
+import de.cau.cs.kieler.klighd.piccolo.svg.SVGGeneratorManager;
+import de.cau.cs.kieler.klighd.piccolo.svg.SVGOffscreenRenderer;
+import edu.umd.cs.piccolo.util.PBounds;
 
 /**
- * Generic SVG {@link IViewExporter}. Is able to export all svg generators registered by the
- * {@link de.cau.cs.kieler.klighd.piccolo.svg.SVGGeneratorManager
+ * Generic SVG {@link KlighdCanvasExporter}. It is able to export SVGs by means of all SVG
+ * generators registered via the {@link de.cau.cs.kieler.klighd.piccolo.svg.SVGGeneratorManager
  * SVGGeneratorManager#EXTP_ID_SVGGENERATORS} extension point.
  * 
  * In order to work, the 'subFormat' ID of a registered exporter has to match the specified ID in
  * the 'svgGenerators' extension point.
  * 
  * @author uru
+ * @author chsch
  */
 public class SVGExporter extends KlighdCanvasExporter {
 
@@ -40,9 +50,30 @@ public class SVGExporter extends KlighdCanvasExporter {
             final boolean cameraViewport, final int scale, final boolean textAsShapes,
             final boolean embedFonts, final String subFormatId) {
 
-        // just call the svg canvas's static render method, pass the subFormat, the canvas knows how
-        // to handle it
-        PCamera camera = canvas.getCamera();
-        KlighdSVGCanvas.render(camera, cameraViewport, textAsShapes, embedFonts, stream, subFormatId);
+        // reveal the canvas' camera ...
+        final KlighdMainCamera camera = canvas.getCamera();
+
+        // ... an determine the bounds of the diagram to be exported
+        final PBounds bounds = getExportedBounds(camera, cameraViewport);
+        
+        // initialize a graphics object that 'collects' all the drawing instructions 
+        final KlighdAbstractSVGGraphics graphics =
+                SVGGeneratorManager.createGraphics(subFormatId, bounds, textAsShapes, embedFonts);
+
+        // do the action diagram drawing work
+        drawDiagram(camera, cameraViewport, graphics, bounds); 
+
+        try {
+            // dump out the SVG description via the provided output stream
+            graphics.stream(stream);
+
+        } catch (final IOException e) {
+            final String msg = "KLighD SVG export: "
+                    + "Failed to write SVG data into the provided OutputStream of type "
+                    + stream.getClass().getCanonicalName() + KlighdPlugin.LINE_SEPARATOR
+                    + " the stream instance is " + stream.toString();
+            StatusManager.getManager().handle(
+                    new Status(IStatus.ERROR, SVGOffscreenRenderer.PLUGIN_ID, msg, e));
+        }
     }
 }
--- plugins/de.cau.cs.kieler.klighd.piccolo/src/de/cau/cs/kieler/klighd/piccolo/export/KlighdCanvasExporter.java
+++ plugins/de.cau.cs.kieler.klighd.piccolo/src/de/cau/cs/kieler/klighd/piccolo/export/KlighdCanvasExporter.java
@@ -13,7 +13,10 @@
  */
 package de.cau.cs.kieler.klighd.piccolo.export;
 
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import java.io.OutputStream;
+import java.util.List;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -21,8 +24,14 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.statushandlers.StatusManager;
 
 import de.cau.cs.kieler.klighd.IDiagramExporter;
+import de.cau.cs.kieler.klighd.KlighdConstants;
 import de.cau.cs.kieler.klighd.piccolo.KlighdPiccoloPlugin;
+import de.cau.cs.kieler.klighd.piccolo.KlighdSWTGraphics;
 import de.cau.cs.kieler.klighd.piccolo.internal.nodes.KlighdCanvas;
+import de.cau.cs.kieler.klighd.piccolo.internal.nodes.KlighdMainCamera;
+import edu.umd.cs.piccolo.PLayer;
+import edu.umd.cs.piccolo.util.PBounds;
+import edu.umd.cs.piccolo.util.PPaintContext;
 
 /**
  * Abstract implementation of {@link IDiagramExporter} supporting the export of diagrams depicted by
@@ -31,8 +40,13 @@ import de.cau.cs.kieler.klighd.piccolo.internal.nodes.KlighdCanvas;
  * Thus, it treats the {@link Control} in
  * {@link #export(OutputStream, Control, boolean, int, boolean, boolean, String)} as a
  * {@link KlighdCanvas} and redirects to
- * {@link #export(OutputStream, KlighdCanvas, boolean, int, boolean, boolean, String)}, which has to be
- * implemented by concrete subclasses.
+ * {@link #export(OutputStream, KlighdCanvas, boolean, int, boolean, boolean, String)}, which has to
+ * be implemented by concrete subclasses.<br>
+ * <br>
+ * Provides the common methods {@link #getExportedBounds(KlighdMainCamera, boolean)} and
+ * {@link #drawDiagram(KlighdMainCamera, boolean, KlighdSWTGraphics, PBounds)} that are to be
+ * incorporated by concrete implementation in order to achieve consistent behavior amongst all
+ * registered {@link IDiagramExporter IDiagramExporters}.
  * 
  * @author chsch
  */
@@ -82,4 +96,108 @@ public abstract class KlighdCanvasExporter implements IDiagramExporter {
      */
     public abstract void export(OutputStream stream, KlighdCanvas canvas, boolean cameraViewport,
             int scale, boolean textAsShapes, boolean embedFonts, String subFormatId);
+
+    
+    /**
+     * Helper method computing the actual unadjusted bounds of the content of the diagram to be
+     * exported.
+     * 
+     * @param camera
+     *            the {@link KlighdMainCamera} whose depicted content is to be exported
+     * @param exportViewport
+     *            if <code>true</code> the camera's view port is considered,
+     *            otherwise the closure bounds of the camera's displayed layer is considered 
+     * @return a new {@link PBounds} instance containing the requested bounds data
+     */
+    protected PBounds getExportedBounds(final KlighdMainCamera camera, final boolean exportViewport) {
+        final PBounds bounds;
+
+        if (exportViewport) {
+            bounds = camera.getBounds();
+
+        } else {
+            final PLayer displayedLayer = camera.getDisplayedLayer();
+
+            if (displayedLayer.getParent() == camera.getRoot()) {
+                // in case the displayed layer is the topNode, invalidate its full bounds
+                //  as they are not required to be up to date for the interactive on screen rendering
+                displayedLayer.invalidateFullBounds();
+                bounds = displayedLayer.getFullBounds();
+
+            } else {
+                bounds = displayedLayer.getUnionOfChildrenBounds(null);
+            }
+        }
+        return bounds;
+    }
+
+    /**
+     * Does the actual diagram rendering work by means of the employed {@link KlighdSWTGraphics}.<br>
+     * This method is supposed to be used by all registered {@link IDiagramExporter
+     * IDiagramExporters} in order to achieve consistent exporting behavior.
+     * 
+     * @param camera
+     *            the {@link KlighdMainCamera} showing the diagram to be exported
+     * @param exportViewport
+     *            if <code>true</code> the camera's view port is exported, otherwise the camera's
+     *            displayed layer is exported
+     * @param graphics
+     *            the graphics object to 'draw' the diagram on
+     * @param bounds
+     *            the of the diagram to be exported, required for determining the main clip and the
+     *            background coloring; may be <code>null</code>,
+     *            {@link #getExportedBounds(KlighdMainCamera, boolean)} will be called in that case
+     */
+    protected void drawDiagram(final KlighdMainCamera camera, final boolean exportViewport,
+            final KlighdSWTGraphics graphics, final PBounds bounds) {
+
+        final PBounds theBounds;
+        if (bounds != null) {
+            theBounds = bounds; 
+        } else {
+            theBounds = getExportedBounds(camera, exportViewport);
+        }
+
+        // adjust the zero reference point
+        graphics.transform(AffineTransform.getTranslateInstance(-theBounds.getX(), -theBounds.getY()));
+
+        // The global clip setting is required as (in PPaintContext) a default one will be set!
+        // This however will let various  browsers go crazy and don't show anything!
+        graphics.setClip(theBounds);
+
+        // explicitly initialize the white background (required especially for SVG exports)  
+        graphics.setFillColor(KlighdConstants.WHITE);
+        graphics.fill(theBounds);
+
+        final PPaintContext paintContext = new PPaintContext((Graphics2D) graphics);
+
+        // the following setting contradict the defaults in BatikSVGGraphics
+        //  which leads to a blown-up svg file with a huge amount of repeated local style settings
+        // therefore, here and in KlighdAbstractSVGGraphics#setRenderingHint(Key, Object)
+        //  the propagation of such RenderingHints has been suppressed
+
+        // paintContext.setRenderQuality(PPaintContext.LOW_QUALITY_RENDERING);
+
+        final PLayer exportedLayer = camera.getDisplayedLayer();
+
+        // perform the painting
+        if (exportViewport) {
+            // only render the current viewport
+            camera.fullPaint(paintContext);
+
+        } else if (exportedLayer != null) {
+            paintContext.pushCamera(camera);
+            paintContext.pushTransform(exportedLayer.getInverseTransform());
+
+            exportedLayer.fullPaint(paintContext);
+
+        } else {
+            // the fallback case, should not happen in context of KLighD
+            @SuppressWarnings("unchecked")
+            final List<PLayer> layersReference = camera.getLayersReference();
+            for (final PLayer layer : layersReference) {
+                layer.fullPaint(paintContext);
+            }
+        }
+    }
 }
