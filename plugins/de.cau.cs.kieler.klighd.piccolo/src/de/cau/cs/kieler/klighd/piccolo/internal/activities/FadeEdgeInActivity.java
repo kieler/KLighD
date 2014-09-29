583f9d113a8a101340c95f393bb50f64aee74d8e
Christian Schneider <chsch@informatik.uni-kiel.de>
2014-09-29 15:58:02 +0200

    klighd.piccolo: fixed checkstyle warning in FadeEdgeInActivity

--- plugins/de.cau.cs.kieler.klighd.piccolo/src/de/cau/cs/kieler/klighd/piccolo/internal/activities/FadeEdgeInActivity.java
+++ plugins/de.cau.cs.kieler.klighd.piccolo/src/de/cau/cs/kieler/klighd/piccolo/internal/activities/FadeEdgeInActivity.java
@@ -16,13 +16,12 @@ package de.cau.cs.kieler.klighd.piccolo.internal.activities;
 import java.awt.geom.Point2D;
 
 import de.cau.cs.kieler.klighd.piccolo.internal.nodes.KEdgeNode;
-import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.activities.PInterpolatingActivity;
 
 /**
  * A custom {@link edu.umd.cs.piccolo.activities.PInterpolatingActivity PInterpolatingActivity} that
- * fades {@link PNode PNodes} representing {@link de.cau.cs.kieler.core.kgraph.KEdge KEdges} into
- * diagrams.
+ * fades {@link edu.umd.cs.piccolo.PNode PNodes} representing
+ * {@link de.cau.cs.kieler.core.kgraph.KEdge KEdges} into diagrams.
  * 
  * @author chsch
  */
@@ -64,6 +63,7 @@ public class FadeEdgeInActivity extends PInterpolatingActivity implements IStart
      * This customization initializes the new node's position, set it transparent, and releases the
      * invisibility.
      */
+    @Override
     public void activityStarted() {
         if (edgeNode.getRenderingController() != null) {
             edgeNode.getRenderingController().modifyStyles();
@@ -90,6 +90,7 @@ public class FadeEdgeInActivity extends PInterpolatingActivity implements IStart
      * <br>
      * This customization fully exposes the given edge.
      */
+    @Override
     public void activityFinished() {
         edgeNode.setTransparency(1);
         super.activityFinished();
