07518b3c0eb3a2352b40b8d38642fcc7e19d3967
Christian Schneider <chsch@informatik.uni-kiel.de>
2015-05-18 13:38:17 +0200

    klighd.ui.wizard: added 'contrib3x' bundle to the ones required by the generated 'KLighD project' in order to have, e.g., the klighd preference page available

--- plugins/de.cau.cs.kieler.klighd.ui.wizard/src/de/cau/cs/kieler/klighd/ui/wizard/KlighdWizardSetup.java
+++ plugins/de.cau.cs.kieler.klighd.ui.wizard/src/de/cau/cs/kieler/klighd/ui/wizard/KlighdWizardSetup.java
@@ -51,7 +51,8 @@ public final class KlighdWizardSetup {
             "javax.inject", "com.google.guava", "com.google.inject", "org.eclipse.xtext.xbase.lib",
             "de.cau.cs.kieler.core.krendering", "de.cau.cs.kieler.core.krendering.extensions",
             "de.cau.cs.kieler.kiml", "de.cau.cs.kieler.klay.layered",
-            "de.cau.cs.kieler.klighd", "de.cau.cs.kieler.klighd.ui", "de.cau.cs.kieler.klighd.piccolo",
+            "de.cau.cs.kieler.klighd", "de.cau.cs.kieler.klighd.piccolo",
+            "de.cau.cs.kieler.klighd.ui", "de.cau.cs.kieler.klighd.ui.contrib3x",
             "de.cau.cs.kieler.kiml.graphviz.layouter;resolution:=optional",
             "org.eclipse.ui", // required for the menu contribution extension points
             "org.eclipse.ui.ide.application", "org.eclipse.ui.navigator.resources",
