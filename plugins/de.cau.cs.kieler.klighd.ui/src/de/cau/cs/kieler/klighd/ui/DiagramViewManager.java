kieler/pragmatics

commit ff733ddb7c1a3ba6003a55225547d1ec189fb12a

klighd +.ui: added a bunch of 'updateDiagram(...)' methods to 'LightDiagramServices', changed some methods of 'DiagramViewManager' to call the new ones

plugins/de.cau.cs.kieler.klighd.ui/src/de/cau/cs/kieler/klighd/ui/DiagramViewManager.java
@@ -190,51 +190,25 @@ public final class DiagramViewManager implements IPartListener {
             final Object model, final IPropertyHolder properties) {
 
         // get the view
-        final IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-        final IWorkbenchPage page = window.getActivePage();
-        ViewContext viewContext = getInstance().idContextMapping.get(id);
+        final IDiagramWorkbenchPart diagramView = getDiagramWorkbenchPart(id);
+        if (diagramView == null) {
+            return null;
 
-        IDiagramWorkbenchPart diagramView = getView(id);
+        } else if (name != null && diagramView instanceof DiagramViewPart) {
+            // set/update the view name
+            ((DiagramViewPart) diagramView).setName(name);
+        } 
 
-        if (diagramView != null) {
-            // set the view name
-            if (name != null) {
-                ((DiagramViewPart) diagramView).setName(name);
-            }
-        } else {
-            diagramView = getEditor(id);
-            if (diagramView == null) {
-                return null;
-            }
-        }
+        final ViewContext viewContext = diagramView.getViewContext();
 
-        // 'diagramView' is supposed to be non-null here
         if (viewContext == null) {
-            viewContext = diagramView.getViewer().getViewContext();
-            if (viewContext == null) {
-                return null;
-            }
+            return null;
         }
 
         // update the view context
-        final Object currentInputModel = viewContext.getInputModel();
-        if (model != null || currentInputModel != null) {
-            page.bringToTop(diagramView);
-            // update the view context and viewer
-            final Object theModel = (model != null ? model : currentInputModel);
-
-            viewContext.getLayoutRecorder().startRecording();
-            final boolean successful = viewContext.update(theModel, properties);
-
-            // in case the view update didn't work properly
-            //  consider this as a failure according to the method doc!
-            if (!successful) {
-                return null;
-            }
-
-            LightDiagramServices.layoutDiagram(viewContext);
+        if (!LightDiagramServices.updateDiagram(viewContext)) {
+            return null;
         }
-
 
         // trigger the update status
         KlighdPlugin.getTrigger().triggerStatus(IKlighdTrigger.Status.UPDATE, viewContext);
@@ -269,38 +243,8 @@ public final class DiagramViewManager implements IPartListener {
      * @return the view with the identifier or null on failure
      */
     public static IDiagramWorkbenchPart updateView(final ViewContext viewContext, final Object model) {
-        // update the view context
-
-        final IDiagramWorkbenchPart diagramView = viewContext.getDiagramWorkbenchPart();
-        final IWorkbenchPage page;
-
-        if (diagramView instanceof DiagramViewPart) {
-            page = diagramView.getSite().getPage();
-        } else if (diagramView instanceof DiagramEditorPart) {
-            page = diagramView.getSite().getPage();
-        } else {
-            return null;
-        }
-
-        final Object currentInputModel = viewContext.getInputModel();
-        if (model != null || currentInputModel != null) {
-            page.bringToTop(diagramView);
-
-            // update the view context and viewer
-            final Object theModel = (model != null ? model : currentInputModel);
-
-            viewContext.getLayoutRecorder().startRecording();
-            final boolean successful = viewContext.update(theModel);
-
-            // in case the view update didn't work properly
-            //  consider this as a failure according to the method doc!
-            if (!successful) {
-                return null;
-            }
-
-            LightDiagramServices.layoutDiagram(viewContext);
-        }
-        return diagramView;
+        boolean res = LightDiagramServices.updateDiagram(viewContext, model);
+        return res ? viewContext.getDiagramWorkbenchPart() : null;
     }
 
     /**

plugins/de.cau.cs.kieler.klighd/src/de/cau/cs/kieler/klighd/LightDiagramServices.java
@@ -61,6 +61,181 @@ public final class LightDiagramServices {
         // do nothing
     }
 
+
+    /**
+     * Updates the diagram represented by the given {@link ViewContext}.<br>
+     * <br>
+     * The <code>viewContext</code>'s current input model is re-evaluated.
+     *
+     * @param viewContext
+     *            the {@link ViewContext} of the diagram to be updated
+     * @return <code>true</code> if update could be performed successfully, <code>false</code>
+     *         otherwise
+     */
+    public static boolean updateDiagram(final ViewContext viewContext) {
+        return updateDiagram(viewContext, null, null, null);
+    }
+
+    /**
+     * Updates the diagram represented by the given {@link ViewContext}.<br>
+     * <br>
+     * The <code>viewContext</code>'s current input model is re-evaluated.
+     *
+     * @param viewContext
+     *            the {@link ViewContext} of the diagram to be updated
+     * @param animate
+     *            layout with or without animation
+     * @return <code>true</code> if update could be performed successfully, <code>false</code>
+     *         otherwise
+     */
+    public static boolean updateDiagram(final ViewContext viewContext, final boolean animate) {
+        return updateDiagram(viewContext, null, null, animate);
+    }
+    
+    /**
+     * Updates the diagram represented by the given {@link ViewContext}.<br>
+     * <br>
+     * The <code>viewContext</code>'s current input model replaced by <code>model</code>.
+     *
+     * @param viewContext
+     *            the {@link ViewContext} of the diagram to be updated
+     * @param model
+     *            the new model, if <code>null</code> the current input model is taken
+     * @return <code>true</code> if update could be performed successfully, <code>false</code>
+     *         otherwise
+     */
+    public static boolean updateDiagram(final ViewContext viewContext, final Object model) {
+        return updateDiagram(viewContext, null, null, null);
+    }
+
+    /**
+     * Updates the diagram represented by the given {@link ViewContext}.<br>
+     * <br>
+     * The <code>viewContext</code>'s current input model is re-evaluated.
+     * 
+     * @param viewContext
+     *            the {@link ViewContext} of the diagram to be updated
+     * @param properties
+     *            an {@link IPropertyHolder} that may influence the diagram update, e.g. via the
+     *            {@link KlighdSynthesisProperties#REQUESTED_UPDATE_STRATEGY} property configuration
+     * @return <code>true</code> if update could be performed successfully, <code>false</code>
+     *         otherwise
+     */
+    public static boolean updateDiagram(final ViewContext viewContext,
+            final IPropertyHolder properties) {
+        return updateDiagram(viewContext, null, properties, null);
+    }
+
+    /**
+     * Updates the diagram represented by the given {@link ViewContext}.<br>
+     * <br>
+     * The <code>viewContext</code>'s current input model replaced by <code>model</code>.
+     * 
+     * @param viewContext
+     *            the {@link ViewContext} of the diagram to be updated
+     * @param model
+     *            the new model, if <code>null</code> the current input model is taken
+     * @param animate
+     *            layout with or without animation
+     * @return <code>true</code> if update could be performed successfully, <code>false</code>
+     *         otherwise
+     */
+    public static boolean updateDiagram(final ViewContext viewContext, final Object model,
+            final boolean animate) {
+        return updateDiagram(viewContext, model, null, animate);
+    }
+    
+    /**
+     * Updates the diagram represented by the given {@link ViewContext}.<br>
+     * <br>
+     * The <code>viewContext</code>'s current input model replaced by <code>model</code>.
+     * 
+     * @param viewContext
+     *            the {@link ViewContext} of the diagram to be updated
+     * @param model
+     *            the new model, if <code>null</code> the current input model is taken
+     * @param properties
+     *            an {@link IPropertyHolder} that may influence the diagram update, e.g. via the
+     *            {@link KlighdSynthesisProperties#REQUESTED_UPDATE_STRATEGY} property configuration
+     * @return <code>true</code> if update could be performed successfully, <code>false</code>
+     *         otherwise
+     */
+    public static boolean updateDiagram(final ViewContext viewContext, final Object model,
+            final IPropertyHolder properties) {
+        return updateDiagram(viewContext, model, properties, null);
+    }
+
+    /**
+     * Updates the diagram represented by the given {@link ViewContext}.<br>
+     * <br>
+     * The <code>viewContext</code>'s current input model is re-evaluated.
+     * 
+     * @param viewContext
+     *            the {@link ViewContext} of the diagram to be updated
+     * @param properties
+     *            an {@link IPropertyHolder} that may influence the diagram update, e.g. via the
+     *            {@link KlighdSynthesisProperties#REQUESTED_UPDATE_STRATEGY} property configuration
+     * @param animate
+     *            layout with or without animation
+     * @return <code>true</code> if update could be performed successfully, <code>false</code>
+     *         otherwise
+     */
+    public static boolean updateDiagram(final ViewContext viewContext,
+            final IPropertyHolder properties, final boolean animate) {
+        return updateDiagram(viewContext, null, properties, animate);
+    }
+
+    /**
+     * Updates the diagram represented by the given {@link ViewContext}.<br>
+     * <br>
+     * The <code>viewContext</code>'s current input model replaced by <code>model</code>.
+     * 
+     * @param viewContext
+     *            the {@link ViewContext} of the diagram to be updated
+     * @param model
+     *            the new model, if <code>null</code> the current input model is taken
+     * @param properties
+     *            an {@link IPropertyHolder} that may influence the diagram update, e.g. via the
+     *            {@link KlighdSynthesisProperties#REQUESTED_UPDATE_STRATEGY} property configuration
+     * @param animate
+     *            layout with or without animation
+     * @return <code>true</code> if update could be performed successfully, <code>false</code>
+     *         otherwise
+     */
+    public static boolean updateDiagram(final ViewContext viewContext, final Object model,
+            final IPropertyHolder properties, final Boolean animate) {
+
+        final Object currentInputModel = viewContext.getInputModel();
+        if (model == null && currentInputModel == null) {
+            return false;
+        }
+
+        // update the view context and viewer
+        final Object theModel = (model != null ? model : currentInputModel);
+
+        viewContext.getLayoutRecorder().startRecording();
+
+        // update the view context
+        final boolean successful = viewContext.update(theModel, properties);
+
+        // in case the view update didn't work properly
+        //  consider this as a failure according to the method doc!
+        if (!successful) {
+            return false;
+        }
+
+        final IDiagramWorkbenchPart diagramWP = viewContext.getDiagramWorkbenchPart();
+
+        if (diagramWP != null) {
+            diagramWP.getSite().getPage().bringToTop(diagramWP);
+        }
+
+        layoutDiagram(null, null, viewContext, animate, null, null, null);
+
+        return true;
+    }
+
+
     /**
      * Performs the automatic layout on the diagram represented by the given view context.<br>
      * <br>

kieler/KLighD

commit 011bf76487167063daf965f8d29e359fd53f2a1d

klighd.ui: solves KIPRA-1702 (fixes a bug in 'DiagramViewManager' introduced in ff733ddb7c1a3ba6003a55225547d1ec189fb12a on May 17, 2015)

plugins/de.cau.cs.kieler.klighd.ui/src/de/cau/cs/kieler/klighd/ui/DiagramViewManager.java
@@ -206,7 +206,7 @@ public final class DiagramViewManager implements IPartListener {
         }
 
         // update the view context
-        if (!LightDiagramServices.updateDiagram(viewContext)) {
+        if (!LightDiagramServices.updateDiagram(viewContext, model, properties)) {
             return null;
         }
 