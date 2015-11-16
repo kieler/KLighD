5c999d1b66c94b0e084284049c6da9518a989c5e
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2015-11-16 15:17:05 +0100

    core.kgraph.text.ui: - Added an option to generate edge labels.

--- plugins/de.cau.cs.kieler.core.kgraph.text.ui/src/de/cau/cs/kieler/core/kgraph/text/ui/random/wizard/Messages.java
+++ plugins/de.cau.cs.kieler.core.kgraph.text.ui/src/de/cau/cs/kieler/core/kgraph/text/ui/random/wizard/Messages.java
@@ -75,6 +75,10 @@ public class Messages extends NLS {
     public static String RandomGraphBipartitePage_partition_error;
     public static String RandomGraphBiconnectedPage_description;
     public static String RandomGraphBiconnectedPage_title;
+
+    public static String RandomGraphCustomPage_edge_labels_caption;
+
+    public static String RandomGraphCustomPage_edge_labels_help;
     public static String RandomGraphNewFilePage_file_format_caption;
     public static String RandomGraphNewFilePage_xtext_format_caption;
     public static String RandomGraphNewFilePage_xtext_format_help;
