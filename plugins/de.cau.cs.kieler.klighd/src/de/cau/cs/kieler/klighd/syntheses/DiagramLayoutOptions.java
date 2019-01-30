0e90fb1f53dc71193c4fb73f2b91002691ff94ed
Christian Schneider <christian.schneider@typefox.io>
2019-01-30 14:22:45 +0100

    klighd: some maintenance in 'DiagramLayoutOptions'
    * added+updated a few constants
    * fixed lots of broken java doc links

--- plugins/de.cau.cs.kieler.klighd/src/de/cau/cs/kieler/klighd/syntheses/DiagramLayoutOptions.java
+++ plugins/de.cau.cs.kieler.klighd/src/de/cau/cs/kieler/klighd/syntheses/DiagramLayoutOptions.java
@@ -16,6 +16,7 @@ package de.cau.cs.kieler.klighd.syntheses;
 import java.util.EnumSet;
 
 import org.eclipse.elk.core.math.ElkMargin;
+import org.eclipse.elk.core.math.ElkPadding;
 import org.eclipse.elk.core.math.KVector;
 import org.eclipse.elk.core.options.BoxLayouterOptions;
 import org.eclipse.elk.core.options.CoreOptions;
@@ -36,7 +37,7 @@ import org.eclipse.elk.graph.properties.IProperty;
 /**
  * A library class listing the typical/relevant layout options to set in diagram synthesis
  * implementations.<br>
- * The offered set may be incomplete, refer to {@link LayoutOptions} for further option definitions.
+ * The offered set may be incomplete, refer to {@link CoreOptions} for further option definitions.
  * However, keep in mind that some of the options defined there are set/overwritten by KLighD
  * internally, and some, e.g. the label concerning ones, are set in the helper functions, e.g. in
  * <code>de.cau.cs.kieler.klighd.krendering.extensions.KLabelExtensions</code>, already.
@@ -62,7 +63,7 @@ public final class DiagramLayoutOptions {
      * Find more layout algorithms and information on their options on our
      * <a href="http://layout.rtsys.informatik.uni-kiel.de:9444/Providedlayout.html">web service</a>.
      *
-     * @see LayoutOptions#ALGORITHM
+     * @see CoreOptions#ALGORITHM
      */
     public static final IProperty<String> ALGORITHM = CoreOptions.ALGORITHM;
 
@@ -77,40 +78,45 @@ public final class DiagramLayoutOptions {
     public static final String FIXED = FixedLayouterOptions.ALGORITHM_ID;
 
     /**
-     * Our default layouter for layer-based diagrams with and without ports, implemented in Java.<br>
+     * The default layouter for layer-based diagrams with and without ports, implemented in Java.<br>
      * Supports {@link EdgeRouting#POLYLINE oblique polyline}, {@link EdgeRouting#ORTHOGONAL
      * orthogonal polyline}, and experimental {@link EdgeRouting#SPLINES spline} edge routing.<br>
      * <br>
      * If you choose this layouter, you might want to try the different node placement strategies we
-     * currently offer. Have a look at the layout options called <code>NODE_PLACER</code> being
-     * defined in <code>de.cau.cs.kieler.klay.layered.Properties</code>, try the different values
-     * offered by <code>de.cau.cs.kieler.klay.layered.p4nodes.NodePlacementStrategy</code>. You need
-     * to add a dependency to <code>de.cau.cs.kieler.klay.layered</code> to your plug-in to access
-     * the aforementioned classes.
+     * currently offer. Have a look at the layout options called <code>NODE_PLACEMENT_STRATEGY</code> being
+     * defined in <code>org.eclipse.elk.alg.layered.options.LayeredOptions</code>, try the different values
+     * offered by <code>org.eclipse.elk.alg.layered.options.NodePlacementStrategy</code>. You need
+     * to add a dependency to the <code>org.eclipse.elk.alg.layered</code> eclipse plug-in or maven artifact
+     * to access the aforementioned classes.
      */
-    public static final String KLAY_LAYERED = "de.cau.cs.kieler.klay.layered";
-
+    public static final String ELK_LAYERED = "org.eclipse.elk.layered";
+    
+    /**
+     * @deprecated use {@link #ELK_LAYERED}
+     */
+    public static final String KLAY_LAYERED = ELK_LAYERED;
+    
     /**
      * Creates layer-based layouts.<br>
-     * Requires the bundle <code>de.cau.cs.kieler.kiml.graphviz.layouter</code> and <a
+     * Requires the bundle <code>org.eclipse.elk.alg.graphviz.layouter</code> and <a
      * href="http://www.graphviz.org/">Graphviz</a> to be installed.<br>
      * Supports {@link EdgeRouting#POLYLINE oblique polyline}, {@link EdgeRouting#ORTHOGONAL
      * orthogonal polyline}, and {@link EdgeRouting#SPLINES spline} edge routing.
      */
-    public static final String GRAPHVIZ_DOT = "de.cau.cs.kieler.graphviz.dot";
+    public static final String GRAPHVIZ_DOT = "org.eclipse.elk.graphviz.dot";
 
     /**
      * Creates circular layouts.<br>
-     * Requires the bundle <code>de.cau.cs.kieler.graphviz.layouter</code> and <a
+     * Requires the bundle <code>org.eclipse.elk.alg.graphviz.layouter</code> and <a
      * href="http://www.graphviz.org/">Graphviz</a> to be installed.
      */
-    public static final String GRAPHVIZ_CIRCO = "de.cau.cs.kieler.graphviz.circo";
+    public static final String GRAPHVIZ_CIRCO = "org.eclipse.elk.graphviz.circo";
 
     /**
      * Dedicated to Class-Diagram-like diagrams.<br>
      * Requires the bundle <code>de.cau.cs.kieler.kiml.ogdf</code>.
      */
-    public static final String OGDF_PLANARIZATION = "de.cau.cs.kieler.ogdf.planarization";
+    public static final String OGDF_PLANARIZATION = "de.cau.cs.kieler.kiml.ogdf.planarization";
     /**
      * Creates circular layouts.<br>
      * Requires the bundle <code>de.cau.cs.kieler.kiml.ogdf</code>.
@@ -119,108 +125,113 @@ public final class DiagramLayoutOptions {
 
 
     /**
-     * @see LayoutOptions#ADDITIONAL_PORT_SPACE;
+     * @see CoreOptions#ADDITIONAL_PORT_SPACE;
      */
     public static final IProperty<ElkMargin> ADDITIONAL_PORT_SPACE =
             CoreOptions.SPACING_PORTS_SURROUNDING;
 
     /**
-     * @see LayoutOptions#DIRECTION
+     * @see CoreOptions#DIRECTION
      */
     public static final IProperty<Direction> DIRECTION = CoreOptions.DIRECTION;
 
     /**
-     * @see LayoutOptions#EDGE_ROUTING
+     * @see CoreOptions#EDGE_ROUTING
      */
     public static final IProperty<EdgeRouting> EDGE_ROUTING = CoreOptions.EDGE_ROUTING;
 
     /**
-     * @see LayoutOptions#LABEL_SPACING
+     * @see CoreOptions#LABEL_SPACING
      */
     public static final IProperty<Double> LABEL_SPACING = CoreOptions.SPACING_LABEL_LABEL;
 
     /**
-     * @see LayoutOptions#HIERARCHY_HANDLING
+     * @see CoreOptions#HIERARCHY_HANDLING
      */
     public static final IProperty<HierarchyHandling> HIERARCHY_HANDLING =
             CoreOptions.HIERARCHY_HANDLING;
 
     /**
-     * @see LayoutOptions#OFFSET
+     * @see CoreOptions#OFFSET
      */
     public static final IProperty<Double> OFFSET = CoreOptions.PORT_BORDER_OFFSET;
 
     /**
-     * @see LayoutOptions#PORT_ALIGNMENT
+     * @see CoreOptions#PORT_ALIGNMENT
      */
     public static final IProperty<PortAlignment> PORT_ALIGNMENT = CoreOptions.PORT_ALIGNMENT_DEFAULT;
 
     /**
-     * @see LayoutOptions#PORT_ALIGNMENT_EAST
+     * @see CoreOptions#PORT_ALIGNMENT_EAST
      */
     public static final IProperty<PortAlignment> PORT_ALIGNMENT_EAST = CoreOptions.PORT_ALIGNMENT_EAST;
 
     /**
-     * @see LayoutOptions#PORT_ALIGNMENT_NORTH
+     * @see CoreOptions#PORT_ALIGNMENT_NORTH
      */
     public static final IProperty<PortAlignment> PORT_ALIGNMENT_NORTH =
             CoreOptions.PORT_ALIGNMENT_NORTH;
 
     /**
-     * @see LayoutOptions#PORT_ALIGNMENT_SOUTH
+     * @see CoreOptions#PORT_ALIGNMENT_SOUTH
      */
     public static final IProperty<PortAlignment> PORT_ALIGNMENT_SOUTH =
             CoreOptions.PORT_ALIGNMENT_SOUTH;
 
     /**
-     * @see LayoutOptions#PORT_ALIGNMENT_WEST
+     * @see CoreOptions#PORT_ALIGNMENT_WEST
      */
     public static final IProperty<PortAlignment> PORT_ALIGNMENT_WEST = CoreOptions.PORT_ALIGNMENT_WEST;
 
     /**
-     * @see LayoutOptions#PORT_ANCHOR
+     * @see CoreOptions#PORT_ANCHOR
      */
     public static final IProperty<KVector> PORT_ANCHOR = CoreOptions.PORT_ANCHOR;
 
     /**
-     * @see LayoutOptions#PORT_CONSTRAINTS
+     * @see CoreOptions#PORT_CONSTRAINTS
      */
     public static final IProperty<PortConstraints> PORT_CONSTRAINTS = CoreOptions.PORT_CONSTRAINTS;
 
     /**
-     * @see LayoutOptions#PORT_INDEX
+     * @see CoreOptions#PORT_INDEX
      */
     public static final IProperty<Integer> PORT_INDEX = CoreOptions.PORT_INDEX;
 
     /**
-     * @see LayoutOptions#PORT_LABEL_PLACEMENT
+     * @see CoreOptions#PORT_LABEL_PLACEMENT
      */
     public static final IProperty<PortLabelPlacement> PORT_LABEL_PLACEMENT =
             CoreOptions.PORT_LABELS_PLACEMENT;
 
     /**
-     * @see LayoutOptions#PORT_SPACING
+     * @see CoreOptions#PORT_SPACING
      */
     public static final IProperty<Double> PORT_SPACING = CoreOptions.SPACING_PORT_PORT;
 
     /**
-     * @see LayoutOptions#PORT_SIDE
+     * @see CoreOptions#PORT_SIDE
      */
     public static final IProperty<PortSide> PORT_SIDE = CoreOptions.PORT_SIDE;
 
     /**
-     * @see LayoutOptions#SIZE_CONSTRAINT
+     * @see CoreOptions#SIZE_CONSTRAINT
      */
     public static final IProperty<EnumSet<SizeConstraint>> SIZE_CONSTRAINT =
             CoreOptions.NODE_SIZE_CONSTRAINTS;
 
     /**
-     * @see LayoutOptions#SIZE_OPTIONS
+     * @see CoreOptions#SIZE_OPTIONS
      */
     public static final IProperty<EnumSet<SizeOptions>> SIZE_OPTIONS = CoreOptions.NODE_SIZE_OPTIONS;
 
     /**
-     * @see LayoutOptions#SPACING
+     * @see CoreOptions#PADDING
+     */
+    public static final IProperty<ElkPadding> PADDING = CoreOptions.PADDING;
+
+    /**
+     * @see CoreOptions#SPACING_NODE_NODE
      */
     public static final IProperty<Double> SPACING = CoreOptions.SPACING_NODE_NODE;
 
