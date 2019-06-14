b4aa4174d218a98dfff91b4ddfe6a86de9ca8216
Niklas Rentz <nre@informatik.uni-kiel.de>
2019-06-14 12:19:43 +0200

    klighd: more documentation for the AbstractSubSynthesis.

--- plugins/de.cau.cs.kieler.klighd/src/de/cau/cs/kieler/klighd/syntheses/AbstractSubSynthesis.java
+++ plugins/de.cau.cs.kieler.klighd/src/de/cau/cs/kieler/klighd/syntheses/AbstractSubSynthesis.java
@@ -25,22 +25,46 @@ import de.cau.cs.kieler.klighd.kgraph.KGraphElement;
 import de.cau.cs.kieler.klighd.krendering.ViewSynthesisShared;
 
 /** 
- * Abstract class for partial syntheses, delegating helper methods.
- * Can be used within any other diagram synthesis or sub-synthesis via injection.
+ * Abstract class for partial syntheses, delegating helper methods to be usable like a AbstractDiagramSynthesis.
+ * A non-abstract subclass of this can be used within any other diagram synthesis or sub-synthesis via injection.
+ * An example parent synthesis should use subclass of this abstract sub synthesis as follows (Xtend code):
+ * <pre>
+ *&#64;ViewSynthesisShared
+ *class ExampleSynthesis extends AbstractDiagramSynthesis&#60;ModelToVisualize&#62; {
+ *    // Alternatively: extends AbstractSubSynthesis&#60;SubModel, KNode&#62;
+ *    &#64;Inject OtherSubSynthesis subSynthesis
+ *    
+ *    override transform(ModelToVisualize model) {
+ *        [...]
+ *        val subModel = subSynthesis.transform(model.submodel)
+ *        [...]
+ *    }
+ *
+ *}
+ * </pre>
+ * for some model class ModelToVisualize with a submodel and some sub synthesis OtherSubSynthesis extending this class
+ * for that submodel.
  * 
  * @param <I> Input type of the sub-synthesis.
  * @param <O> Output type of the sub-synthesis.
  * @author nre
+ * @see AbstractDiagramSynthesis
  */
 @ViewSynthesisShared
 public abstract class AbstractSubSynthesis<I, O extends KGraphElement> {
     
+    /**
+     * The parent synthesis that helper methods are delegated to.
+     * This will always be the instance of the parent synthesis as it is the only synthesis known to the injector here.
+     * The &#64;{@link ViewSynthesisShared} annotation makes sure it is used in singleton scope and this injected
+     * synthesis really is the parent synthesis that injected this sub synthesis.
+     */
     @Inject private AbstractDiagramSynthesis<?> parent;
 
     /** 
-     * Transforms the given element into a diagram element.
+     * Transforms the given element into a list of diagram elements.
      * @param element The model element to transform.
-     * @return The transformed diagram element.
+     * @return The transformed diagram elements.
      */
     public abstract List<O> transform(I element);
 
