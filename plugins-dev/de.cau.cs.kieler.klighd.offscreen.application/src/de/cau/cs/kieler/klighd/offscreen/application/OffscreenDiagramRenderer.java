013d6687ead7fee7041eff36f98e02375e5b2b56
Niklas Rentz <47597619+NiklasRentzCAU@users.noreply.github.com>
2021-11-10 12:58:03 +0100

    all: upgraded to EPL-2.0 license. (#96)

--- plugins-dev/de.cau.cs.kieler.klighd.offscreen.application/src/de/cau/cs/kieler/klighd/offscreen/application/OffscreenDiagramRenderer.java
+++ plugins-dev/de.cau.cs.kieler.klighd.offscreen.application/src/de/cau/cs/kieler/klighd/offscreen/application/OffscreenDiagramRenderer.java
@@ -8,8 +8,11 @@
  *   + Department of Computer Science
  *     + Real-Time and Embedded Systems Group
  * 
- * This code is provided under the terms of the Eclipse Public License (EPL).
- * See the file epl-v10.html for the license text.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
  */
 
 package de.cau.cs.kieler.klighd.offscreen.application;
