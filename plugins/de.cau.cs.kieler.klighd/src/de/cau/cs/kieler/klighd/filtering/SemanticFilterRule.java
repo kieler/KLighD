4a781d022eeb3f39f3cb0e1ed31fe974e9bf519b
Max Kasperowski <mka@informatik.uni-kiel.de>
2022-06-23 16:47:06 +0200

    add basic semantic structure to create semantic filters

---
+++ plugins/de.cau.cs.kieler.klighd/src/de/cau/cs/kieler/klighd/filtering/SemanticFilterRule.java
@@ -0,0 +1,25 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://rtsys.informatik.uni-kiel.de/kieler
+ * 
+ * Copyright 2022 by
+ * + Kiel University
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package de.cau.cs.kieler.klighd.filtering;
+
+/**
+ * @author mka
+ *
+ */
+public abstract class SemanticFilterRule {
+
+}
