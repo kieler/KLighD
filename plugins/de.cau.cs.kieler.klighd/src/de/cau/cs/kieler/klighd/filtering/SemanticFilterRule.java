18215bc43fab3195ad9e479c827971a812cbd4a2
Niklas Rentz <nre@informatik.uni-kiel.de>
2023-03-03 11:18:29 +0100

    klighd: moved example code for semantic filtering rules here.

--- plugins/de.cau.cs.kieler.klighd/src/de/cau/cs/kieler/klighd/filtering/SemanticFilterRule.java
+++ plugins/de.cau.cs.kieler.klighd/src/de/cau/cs/kieler/klighd/filtering/SemanticFilterRule.java
@@ -19,6 +19,7 @@ package de.cau.cs.kieler.klighd.filtering;
 /**
  * Semantic filter rules describe a filter that can be evaluated to include only elements tagged with certain tags.
  * The rules are constructed through the combination of filter rules with logical connectives.
+ * Complex example filters are provided in the comments of the source code below.
  * 
  * @author mka
  * @author tik
@@ -83,4 +84,66 @@ public abstract class SemanticFilterRule {
     public boolean getDefaultValue() { 
         return this.defaultValue;
     }
+    
+//    /**
+//     * Rule to exclude elements that are either initial XOR final states.
+//     * 
+//     * @example
+//     */
+//    public static final SemanticFilterRule NO_INITIAL_XOR_FINAL_STATE =
+//            new AndConnective(SCChartsSemanticFilterTags.STATE,
+//                    new LogicEqualConnective(SCChartsSemanticFilterTags.INITIAL,
+//                            SCChartsSemanticFilterTags.FINAL),
+//                    "Filter Initial XOR Final States");
+//
+//    /**
+//     * Rule to only include elements that are either initial states or regions.
+//     * 
+//     * @example
+//     */
+//    public static final SemanticFilterRule ONLY_INITIAL_STATES_OR_REGIONS =
+//            new IfThenElseConnective(SCChartsSemanticFilterTags.STATE,
+//                    SCChartsSemanticFilterTags.INITIAL, SCChartsSemanticFilterTags.REGION,
+//                    "Filter Everything but Initial States or Regions");
+//
+//    /** Rule to only include elements that have at least 3 declarations. */
+//    public static final SemanticFilterRule ONLY_AT_LEAST_3_DECLARATIONS =
+//            new GreaterEqualsConnective(SCChartsSemanticFilterTags.DECLARATIONS(null),
+//                    new NumericConstantConnective(3.0),
+//                    "Filter Elements with less than 3 Declarations");
+//
+//    /**
+//     * Rule to only include elements that are initial, final or a region.
+//     * 
+//     * @example
+//     */
+//    public static final SemanticFilterRule ONLY_INITIAL_OR_FINAL_OR_REGION = SemanticFilterRuleUtil
+//            .getBigOrConnective("Filter Everything but Initial, Final or Regions",
+//                    SCChartsSemanticFilterTags.INITIAL, SCChartsSemanticFilterTags.FINAL,
+//                    SCChartsSemanticFilterTags.REGION);
+//
+//    /**
+//     * Rule to exclude every element.
+//     * 
+//     * @example
+//     */
+//    public static final SemanticFilterRule NO_EVERYTHING = new FalseConnective("Filter Everything");
+//
+//    /**
+//     * Rule to only include every element.
+//     * 
+//     * @example
+//     */
+//    public static final SemanticFilterRule ONLY_EVERYTHING = new TrueConnective("Filter Nothing");
+//
+//    /**
+//     * Rule to only include elements with more input/output declarations than the total number of declarations.
+//     * 
+//     * @example
+//     */
+//    public static final SemanticFilterRule ONLY_MORE_IO_THAN_DECLARATIONS = new GreaterThanConnective(
+//            new NumericAdditionConnective(SCChartsSemanticFilterTags.INPUT_DECLARATIONS(null),
+//                    SCChartsSemanticFilterTags.OUTPUT_DECLARATIONS(null)),
+//            SCChartsSemanticFilterTags.DECLARATIONS(null),
+//            "Filter Elements with #IO Declarations <= #Declarations");
 }
